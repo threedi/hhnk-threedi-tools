cdaeca55abf199ed0a386bd015f9042d9de4b43c
LER1990 <photoravier@gmail.com>
2021-05-16 20:30:01 +0200

    fix: renamed type to test_type (type is built-in)

--- hhnk_wsa_tests/folder_structure_and_paths/build_output_files_dict.py
+++ hhnk_wsa_tests/folder_structure_and_paths/build_output_files_dict.py
@@ -1,6 +1,6 @@
 import os
 
-def build_output_files_dict(type, base_folder, revision_dir_name=None):
+def build_output_files_dict(test_type, base_folder, revision_dir_name=None):
     """
     Creates a dict containing the file paths (without extensions) we will
     use to write the output to files
@@ -13,7 +13,7 @@ def build_output_files_dict(type, base_folder, revision_dir_name=None):
     a types of test (output/sqlite_tests or output/0d1d_tests for example)
     """
     files_dict = {}
-    if type in (2, 4) and revision_dir_name:
+    if test_type in (2, 4) and revision_dir_name:
         # If 3di revisions are involved, we add the revisions name to the output path
         # ex: output/0d1d_tests/{polder name}_#{revision number}_{test type}
         output_revision_dir = revision_dir_name.replace(' ', '_')
@@ -27,7 +27,7 @@ def build_output_files_dict(type, base_folder, revision_dir_name=None):
         files_dict['log_path'] = os.path.join(base_path, 'Logs')
         files_dict['layer_path'] = os.path.join(base_path, 'Layers')
 
-    if type == 1:
+    if test_type == 1:
         add_log_layer_path(base_path=files_dict['output'])
         files_dict['impervious_surface_filename'] = 'ondoorlatend_oppervlak'
         files_dict['profiles_used_filename'] = 'gebruikte_profielen'
@@ -41,20 +41,20 @@ def build_output_files_dict(type, base_folder, revision_dir_name=None):
         files_dict['dewatering_filename'] = 'drooglegging'
         files_dict['water_surface_filename'] = 'oppervlaktewater'
 
-    if type == 2:
+    if test_type == 2:
         add_log_layer_path(base_path=files_dict['output'])
         files_dict['zero_d_one_d_filename'] = '0d1d_toetsing'
         files_dict['hyd_test_channels_filename'] = 'hydraulische_toets_watergangen'
         files_dict['hyd_test_structs_filename'] = 'hydraulische_toets_kunstwerken'
 
-    if type == 3:
+    if test_type == 3:
         add_log_layer_path(base_path=files_dict['output'])
         files_dict['flow_1d2d_flowlines_filename'] = 'stroming_1d2d_flowlines'
         files_dict['flow_1d2d_cross_sections_filename'] = 'stroming_1d2d_cross_sections'
         files_dict['flow_1d2d_channels_filename'] = 'stroming_1d2d_watergangen'
         files_dict['flow_1d2d_manholes_filename'] = 'stroming_1d2d_putten'
 
-    if type == 4:
+    if test_type == 4:
         add_log_layer_path(base_path=files_dict['output'])
         files_dict['grid_nodes_2d_filename'] = 'grid_nodes_2d'
         files_dict['1d2d_all_flowlines_filename'] = '1d2d_alle_stroming'
--- hhnk_wsa_tests/folder_structure_and_paths/get_working_paths.py
+++ hhnk_wsa_tests/folder_structure_and_paths/get_working_paths.py
@@ -2,7 +2,8 @@ import os
 from .build_threedi_paths_dict import build_threedi_source_paths_dict
 from .build_output_files_dict import build_output_files_dict
 
-def get_working_paths(type, active_paths,
+def get_working_paths(test_type,
+                      active_paths,
                       base_folder_output,
                       threedi_results_path=None,
                       threedi_revision_name=None,
@@ -18,9 +19,9 @@ def get_working_paths(type, active_paths,
     if appropriate and generates output paths, output file names and layer names
     """
     source_paths = active_paths.copy()
-    output_dict = build_output_files_dict(type=type,
-                                           base_folder=base_folder_output,
-                                           revision_dir_name=threedi_revision_name)
+    output_dict = build_output_files_dict(test_type=test_type,
+                                          base_folder=base_folder_output,
+                                          revision_dir_name=threedi_revision_name)
     if threedi_results_path is not None and threedi_revision_name is not None:
         threedi_revision_path = os.path.join(threedi_results_path, threedi_revision_name)
     if threedi_revision_path is not None:
