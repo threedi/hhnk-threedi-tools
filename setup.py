85ec0868cfbda6ef3068450daf78ccb6b4b5b618
LER1990 <photoravier@gmail.com>
2021-05-17 09:19:56 +0200

    fix: move input verification from plugin to wsa tests

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='hhnk_wsa_tests',
-    version='0.1.12',
+    version='0.1.14',
     description='HHNK watersystemen analyse tests',
     url='https://github.com/LER1990/hhnk_toolbox_universal',
     author='Laure Ravier',
