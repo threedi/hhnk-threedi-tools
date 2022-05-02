0b3c194ae47dfc5358ee8c91dca4d29e2738e5e9
Ckerklaan <38913567+Ckerklaan@users.noreply.github.com>
2022-05-02 15:04:53 +0200

    Updated the calculation gui's to klondike.
    Calculation gui updated to klondike. Other scripting is deprecated, but still present.

--- hhnk_threedi_tools/__init__.py
+++ hhnk_threedi_tools/__init__.py
@@ -5,9 +5,8 @@ import sys
 # sys.path.insert(0, 'C:\\Users\wvangerwen\github\hhnk-threedi-tools')
 
 # folder
-from hhnk_threedi_tools.core.folders import Folders as folders
-from hhnk_threedi_tools.core.folders import Folders as Folders
-
+from osgeo import gdal
+from hhnk_threedi_tools.core.folders import Folders
 
 import hhnk_threedi_tools.core
 import hhnk_threedi_tools.resources
