d74d2169a942904d43eb5148c46549dde04d0ded
Wietse van Gerwen <41104904+wvangerwen@users.noreply.github.com>
2023-05-10 17:02:47 +0200

    deploy username

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 REM install local folder to external deps
 python setup.py bdist_wheel 
 pip uninstall hhnk_threedi_tools -y
-pip install --target %appdata%\3Di\QGIS3\profiles\default\python\plugins\hhnk_threedi_plugin\external-dependencies --upgrade E:\github\wvangerwen\hhnk-threedi-tools\dist\hhnk_threedi_tools-2023.2-py3-none-any.whl
+pip install --target %appdata%\3Di\QGIS3\profiles\default\python\plugins\hhnk_threedi_plugin\external-dependencies --upgrade E:\github\%username%\hhnk-threedi-tools\dist\hhnk_threedi_tools-2023.2-py3-none-any.whl
 
