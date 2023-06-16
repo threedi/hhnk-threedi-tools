9e29b9a62d05119917f329e164379f3349fdee9b
Wietse van Gerwen <41104904+wvangerwen@users.noreply.github.com>
2023-06-16 16:45:31 +0200

    add verify test

--- tests/test_klimaatsommen_prep.py
+++ tests/test_klimaatsommen_prep.py
@@ -8,6 +8,7 @@ import pathlib
 import pandas as pd
 from pathlib import Path
 import shutil
+import pytest
 
 # Local imports
 from hhnk_threedi_tools.core.climate_scenarios.klimaatsommen_prep import KlimaatsommenPrep
@@ -18,17 +19,21 @@ import hhnk_research_tools as hrt
 
 from tests.config import FOLDER_TEST, TEMP_DIR, TEST_DIRECTORY
 
+def test_klimaatsommenprep_verify():
+    """Raises because not all 18 scenarios downloaded"""
+    with pytest.raises(Exception):
+        klimaatsommenprep = KlimaatsommenPrep(folder=FOLDER_TEST,
+            batch_name="batch_test",
+            cfg_file = 'cfg_lizard.cfg',
+            landuse_file = FOLDER_TEST.model.schema_base.rasters.landuse,
+            verify=True
+        )
+
 def test_klimaatsommenprep():
-
-    folder = FOLDER_TEST
-
-    # SCHADESCHATTER_PATH=Path(r"E:\01.basisgegevens\hhnk_schadeschatter") #TODO naar een localsettings oid verplaatsen?
-
-    klimaatsommenprep = KlimaatsommenPrep(folder=folder,
+    klimaatsommenprep = KlimaatsommenPrep(folder=FOLDER_TEST,
         batch_name="batch_test",
         cfg_file = 'cfg_lizard.cfg',
-        landuse_file = folder.model.schema_base.rasters.landuse,
-        # SCHADESCHATTER_PATH=SCHADESCHATTER_PATH
+        landuse_file = FOLDER_TEST.model.schema_base.rasters.landuse,
         verify=False
     )
     
@@ -37,12 +42,12 @@ def test_klimaatsommenprep():
     batch_test = ClimateResult(batch_test)
     shutil.copytree(src=klimaatsommenprep.batch_fd.downloads.piek_glg_T10.netcdf.pl,
                     dst=batch_test.downloads.piek_glg_T10.netcdf.pl)
-
     klimaatsommenprep.batch_fd = batch_test
     
-    
-    klimaatsommenprep.run(overwrite=True, testing=True) #TODO =True, maar test duurt lang
+    #Run test
+    klimaatsommenprep.run(overwrite=True, testing=True)
 
+    #check results
     for raster_type in ["depth_max", "damage_total"]:
         scenario_metadata = pd.read_csv(klimaatsommenprep.info_file[raster_type], sep=";")
         assertion_metadata = pd.read_csv(TEST_DIRECTORY/fr"test_klimaatsommen/{raster_type}_info_expected.csv", sep=";")
