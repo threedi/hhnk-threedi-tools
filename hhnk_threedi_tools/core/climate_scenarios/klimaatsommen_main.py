2b40325be7f4d230fdee1c63bd02b03e83fa660a
wietsevangerwen <41104904+wvangerwen@users.noreply.github.com>
2023-08-03 18:24:11 +0200

    Update klimaatsommen_main.py

--- hhnk_threedi_tools/core/climate_scenarios/klimaatsommen_main.py
+++ hhnk_threedi_tools/core/climate_scenarios/klimaatsommen_main.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from pathlib import Path
 import sys
-import importlib.resources as pkg_resources  # Load resource from package
 import ipywidgets as widgets
 
 import hhnk_threedi_tools as htt
@@ -32,32 +31,78 @@ plt.ioff() #turn off inline plots, only show when asked
 # folder=Folders(r"E:\02.modellen\model_test_v2")
 folder = htt.Folders(r"C:\Users\wiets\Documents\GitHub\hhnk-threedi-tools\tests\data\model_test")
 
-# %%
 
 
 class KlimaatsommenSettings():
-    def __init__(self, caller):
+    def __init__(self, caller, folder):
         self.caller = caller
-        
-
-    def dem_value(self):
-        """Get dem used in ggg model"""
-        self.folder.model.set_modelsplitter_paths()
-        dem_file = self.folder.model.settings_df.loc["1d2d_ggg", "dem_file"]
-        dem = self.folder.model.schema_base.full_path(dem_file)
-        return dem
-
-
-class KlimaatsommenMain():
-    def __init__(self, folder:htt.Folders):
         self.folder = folder
-        self.widgets = KlimaatsommenWidgets(self.folder)
-        self.settings = KlimaatsommenSettings(self,)
+
+        self.freqs = self.load_freqs()
+
+        #Note that this in theory can be not the dem used in the gxg model.
+        self.dem = self.folder.model.schema_base.rasters.dem_50cm
+
+    # @property
+    # def dem(self):
+        """Not used currently, but can be used to get dem used in model. """
+        # self.folder.model.set_modelsplitter_paths()
+        # dem_file = self.folder.model.settings_df.loc["1d2d_ggg", "dem_file"]
+        # dem = self.folder.model.schema_base.full_path(dem_file)
+        # return dem
+
+    
+    @property
+    def batch_fd(self):
+        """folder class of batch fd, needs to be selected in widget"""
+        selected_batch = self.caller.widgets.output_folder_box.value
+        if selected_batch != "":
+            return self.folder.threedi_results.batch[selected_batch]
+        else:
+            raise Exception("Select batch folder")
+    @property
+    def precipitation_zone(self) -> str:
+        """hevig or debilt. Needs to be selected in widget"""
+        selected_zone = self.caller.widgets.precipitation_zone_box.value
+        if selected_zone != "":
+            return selected_zone.split(" ")[0]
+        else:
+            raise Exception("Select neerslagzone") 
+
+    def load_freqs(self):
+        freqs_xlsx = hrt.get_pkg_resource_path(package_resource=htt.resources, 
+                            name="precipitation_frequency.xlsx")
+        freqs = pd.read_excel(freqs_xlsx, engine="openpyxl")
+        return freqs[freqs["dl_name"].notna()]
+
+
+    def create_df_freqs(self):
+        """With selected precip zone match frequencies with scenario"""
+        downloads = self.batch_fd.downloads
+
+        df = pd.DataFrame(downloads.names, columns=["dl_name"])
+        for dl_name in downloads.names:
+            df.loc[df["dl_name"] == dl_name, "depth_max"] = getattr(downloads, dl_name).depth_max.base
+            df.loc[df["dl_name"] == dl_name, "damage_total"] = getattr(downloads, dl_name).damage_total.base
+
+        freqs = self.freqs[["dl_name", "freq_{}_jaar".format(self.precipitation_zone)]].copy()
+        freqs.rename(
+            {"freq_{}_jaar".format(self.precipitation_zone): "freq_jaar"},
+            axis=1,
+            inplace=True,
+        )
+        return df.merge(freqs, on="dl_name")
+
+    
+    def update_settings_after_selection():
+        self.df_freqs = self.create_df_freqs()
+
 
 class KlimaatsommenWidgets():
     """Widgets die helpen bij inputselectie."""
-    def __init__(self, folder):
-        self.folder = folder
+    def __init__(self, caller):
+        self.caller = caller
+        self.folder = self.caller.settings.folder
 
         #Output folder
         output_folder_options = [""] + self.folder.threedi_results.batch.revisions
@@ -75,7 +120,7 @@ class KlimaatsommenWidgets():
                                                  layout=self.item_layout(grid_area='precipitation_zone_label'))
  
         self.precipitation_zone_box = widgets.Select(
-                                    options=["hevig", "debilt"],
+                                    options=["hevig (blauw)", "debilt (groen)"],
                                     rows=2,
                                     disabled=False,
                                     value=None,
@@ -84,10 +129,11 @@ class KlimaatsommenWidgets():
         
         self.dem_label = widgets.Label("DEM:",
                                     layout=self.item_layout(grid_area="dem_label"),)
-        self.dem_text = widgets.Text(self.dem_value().base.split(self.folder.parent.as_posix())[-1],
+
+        self.dem_text = widgets.Text(self.caller.settings.dem.view_name_with_parents(4),
                                      disabled=True,
                                      layout=self.item_layout(grid_area="dem_text"),)
-        
+
 
         self.precip_figure = widgets.Output(layout=self.item_layout(grid_area="precip_figure"))
 
@@ -95,6 +141,7 @@ class KlimaatsommenWidgets():
         with self.precip_figure:
             plt.show(self.fig)
         
+
     def create_precip_figure(self):
         polder_shape = self.folder.source_data.polder_polygon.load()
 
@@ -103,12 +150,11 @@ class KlimaatsommenWidgets():
         precip_zones_raster = hrt.Raster(precip_zones_raster)
         neerslag_array = precip_zones_raster.get_array(band_count=3)
 
-        fig, ax = plt.subplots(figsize=(8, 8))
+        fig, ax = plt.subplots(figsize=(6, 6))
         ax.imshow(neerslag_array, extent=precip_zones_raster.metadata.bounds)
         polder_shape.plot(ax=ax, color='red')
         return fig
     
-
 
     def item_layout(self, width="95%", grid_area="", **kwargs):
         return widgets.Layout(
@@ -141,66 +187,28 @@ class KlimaatsommenWidgets():
                     """,
             ),
         )
-    
-    @property
-    def batch_folder(self):
-        selected_batch = self.output_folder_box.value
-        if selected_batch != "":
-            return self.folder.threedi_results.batch[selected_batch]
-        else:
-            raise Exception("Select batch folder")
-    @property
-    def precipitation_zone(self):
-        selected_zone = self.precipitation_zone_box.value
-        if selected_zone != "":
-            return selected_zone
-        else:
-            raise Exception("Select neerslagzone") 
-    
+
+
+class KlimaatsommenMain():
+    def __init__(self, folder:htt.Folders):
+        self.settings = KlimaatsommenSettings(self, folder)
+        self.widgets = KlimaatsommenWidgets(self)
+
+
 self = KlimaatsommenMain(folder=folder)
 self.widgets.gui()
 
 
 # %%
 
+# %%
 
 
-freqs_xlsx = hrt.get_pkg_resource_path(package_resource=htt.resources, 
-                      name="precipitation_frequency.xlsx")
-freqs = pd.read_excel(freqs_xlsx, engine="openpyxl")
 
 
 
 
 # %%
-
-
-if dem.metadata.pixel_width != 0.5:
-    new_dem_path = batch_fd.downloads.full_path(f"{dem.stem}_50cm.tif")
-    dem = hrt.Raster(new_dem_path)
-
-
-df = pd.DataFrame(batch_fd.downloads.names, columns=["dl_name"])
-for dl_name in batch_fd.downloads.names:
-    df.loc[df["dl_name"] == dl_name, "depth_max"] = getattr(
-        batch_fd.downloads, dl_name
-    ).depth_max.base
-    df.loc[df["dl_name"] == dl_name, "damage_total"] = getattr(
-        batch_fd.downloads, dl_name
-    ).damage_total.base
-
-
-## %%
-
-freqs = freqs[["dl_name", "freq_{}_jaar".format(precipitation_zone_box.value)]]
-freqs.rename(
-    {"freq_{}_jaar".format(precipitation_zone_box.value): "freq_jaar"},
-    axis=1,
-    inplace=True,
-)
-
-df_freqs = df.merge(freqs, on="dl_name")
-
 ## %% Aanmaken of laden peilgebieden polygon
 if not folder.source_data.peilgebieden.peilgebieden.exists():
     fixeddrainage = folder.source_data.datachecker.load("fixeddrainagelevelarea")
@@ -212,3 +220,6 @@ else:
     print(
         f"Peilgebieden shapefile gevonden: {folder.source_data.peilgebieden.peilgebieden.name}.shp"
     )
+
+
+# %%
