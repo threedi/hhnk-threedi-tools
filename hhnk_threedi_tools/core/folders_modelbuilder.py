2dd74185db1b70f5214c5f877a2e06b28d0b989b
wietsevangerwen <41104904+wvangerwen@users.noreply.github.com>
2023-11-09 18:29:07 +0100

    format

--- hhnk_threedi_tools/core/folders_modelbuilder.py
+++ hhnk_threedi_tools/core/folders_modelbuilder.py
@@ -1,19 +1,25 @@
-from dataclasses import dataclass
-import hhnk_research_tools as hrt
+"""Folderstructuur voor de modelbuilder."""
 import os
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Union
+
+import hhnk_research_tools as hrt
 
 
+@dataclass
 class SourcePaths:
-    def __init__(self,
-        dem_path:str,
-        glg_path:str,
-        ggg_path:str,
-        ghg_path:str,
-        polder_path:str,
-        watervlakken_path:str,
-    ):
+    """Locaties van inputbestanden voor het maken van de rasters"""
+
+    dem_path: Union[str, Path, hrt.Raster]
+    glg_path: Union[str, Path, hrt.Raster]
+    ggg_path: Union[str, Path, hrt.Raster]
+    ghg_path: Union[str, Path, hrt.Raster]
+    polder_path: str
+    watervlakken_path: str
+
+    def __post_init__(self):
         """
-        Locaties van inputbestanden voor het maken van de rasters
         dem_path (str): raster source van DEM
         glg_path (str): raster source van glg
         ggg_path (str): raster source van ggg
@@ -21,35 +27,37 @@ class SourcePaths:
         polder_path (str): polder met bounds van de output
         watervlakken_path (str): shapfile met watervlakken van polder
         """
-                
-        self.dem = hrt.Raster(dem_path)
-        self.glg = hrt.Raster(glg_path)
-        self.ggg = hrt.Raster(ggg_path)
-        self.ghg = hrt.Raster(ghg_path)
-        self.polder = hrt.File(polder_path)
-        self.watervlakken = hrt.File(watervlakken_path)
+
+        self.dem = hrt.Raster(self.dem_path)
+        self.glg = hrt.Raster(self.glg_path)
+        self.ggg = hrt.Raster(self.ggg_path)
+        self.ghg = hrt.Raster(self.ghg_path)
+        self.polder = hrt.File(self.polder_path)
+        self.watervlakken = hrt.File(self.watervlakken_path)
         self.verify()
 
     def verify(self):
-        for f in [self.dem, self.glg, self.ggg, 
-                self.ghg, self.polder, self.watervlakken]:
+        """Verifiy check if inputs exist"""
+        for f in [self.dem, self.glg, self.ggg, self.ghg, self.polder, self.watervlakken]:
             if not f.exists():
-                raise Exception(f"{f} not found")
+                raise FileNotFoundError(f"{f} not found")
         return True
 
 
 class FoldersModelbuilder:
-    def __init__(self, dst_path:str, source_paths:SourcePaths):
-        """Folder structuur om de rasters in op te slaan.
-        
-        dst_base (str): path to output folder
-        source_paths (SourcePaths): bronrasters en shapes
-        """
+    """Folder structuur om de rasters in op te slaan.
+
+    dst_base (str): path to output folder
+    source_paths (SourcePaths): bronrasters en shapes
+    """
+
+    def __init__(self, dst_path: str, source_paths: SourcePaths):
         self.src = source_paths
         self.dst = self.DestPaths(dst_path)
 
-
     class DestPaths(hrt.Folder):
+        """Output rasters, heeft temp rasters om de berekening te doen."""
+
         def __init__(self, base):
             super().__init__(base=os.path.join(base, ""))
             self.tmp = self.TempPaths(base)
@@ -58,17 +66,16 @@ class FoldersModelbuilder:
             self.glg = self.full_path("glg.tif")
             self.ggg = self.full_path("ggg.tif")
             self.ghg = self.full_path("ghg.tif")
-            
 
         class TempPaths(hrt.Folder):
+            """temp rasters allemaal met dezelfde extent."""
+
             def __init__(self, base):
-                """temp rasters allemaal met dezelfde extent."""
                 super().__init__(base=os.path.join(base, "tmp"))
 
                 self.dem = self.full_path("dem.vrt")
                 self.glg = self.full_path("glg.vrt")
                 self.ggg = self.full_path("ggg.vrt")
-                self.ghg = self.full_path("ghg.vrt")    
+                self.ghg = self.full_path("ghg.vrt")
                 self.polder = self.full_path("polder.tif")
                 self.watervlakken = self.full_path("watervlakken.tif")
-
--- hhnk_threedi_tools/core/modelbuilder/create_schematisation_rasters.py
+++ hhnk_threedi_tools/core/modelbuilder/create_schematisation_rasters.py
@@ -1,66 +1,66 @@
 # %%
-# Aanmaken rasters voor model
-import hhnk_research_tools as hrt
-from dataclasses import dataclass
-import geopandas as gpd
-import numpy as np
+"""Aanmaken rasters voor model"""
 import os
+from dataclasses import dataclass
+
+import geopandas as gpd
+import hhnk_research_tools as hrt
 import hhnk_research_tools.raster_functions as raster_functions
-import hhnk_threedi_tools.core.folders_modelbuilder as folders_modelbuilder
+import numpy as np
+
 import hhnk_threedi_tools as htt
+import hhnk_threedi_tools.core.folders_modelbuilder as folders_modelbuilder
 
 if __name__ == "__main__":
     from pathlib import Path
+
     TEST_DIRECTORY = Path(__file__).parents[3].absolute() / "tests" / "data"
 
     PATH_TEST_MODEL = TEST_DIRECTORY / "model_test"
-    TEMP_DIR = hrt.Folder(TEST_DIRECTORY/r"temp", create=True)
+    TEMP_DIR = hrt.Folder(TEST_DIRECTORY / r"temp", create=True)
     TEMP_DIR.unlink_contents()
+
 
 # %%
 import importlib
+
 importlib.reload(folders_modelbuilder)
 
-raster_path = hrt.Folder(r"C:\Users\wiets\Documents\HHNK\07.Poldermodellen\10.tHoekje\02. Sqlite modeldatabase\rasters")
+raster_path = hrt.Folder(
+    r"C:\Users\wiets\Documents\HHNK\07.Poldermodellen\10.tHoekje\02. Sqlite modeldatabase\rasters"
+)
 
 source_paths = folders_modelbuilder.SourcePaths(
-    dem_path = raster_path.full_path("dem_thoekje.tif"),
-    glg_path = raster_path.full_path("storage_glg_thoekje.tif"),
-    ggg_path = raster_path.full_path("storage_ggg_thoekje.tif"),
-    ghg_path = raster_path.full_path("storage_ghg_thoekje.tif" ),
-    polder_path = r"C:\Users\wiets\Documents\GitHub\hhnk-threedi-tools\tests\data\model_test\01_source_data\polder_polygon.shp",
-    watervlakken_path = r"C:\Users\wiets\Documents\GitHub\hhnk-threedi-tools\tests\data\model_test\01_source_data\modelbuilder_output\channel_surface_from_profiles.shp"
+    dem_path=raster_path.full_path("dem_thoekje.tif"),
+    glg_path=raster_path.full_path("storage_glg_thoekje.tif"),
+    ggg_path=raster_path.full_path("storage_ggg_thoekje.tif"),
+    ghg_path=raster_path.full_path("storage_ghg_thoekje.tif"),
+    polder_path=r"C:\Users\wiets\Documents\GitHub\hhnk-threedi-tools\tests\data\model_test\01_source_data\polder_polygon.shp",
+    watervlakken_path=r"C:\Users\wiets\Documents\GitHub\hhnk-threedi-tools\tests\data\model_test\01_source_data\modelbuilder_output\channel_surface_from_profiles.shp",
 )
 
-folder = folders_modelbuilder.FoldersModelbuilder(dst_path = TEMP_DIR.base,
-    source_paths=source_paths
-)
+folder = folders_modelbuilder.FoldersModelbuilder(dst_path=TEMP_DIR.base, source_paths=source_paths)
 
 
 # %%
 class ModelbuilderRasters:
-    def __init__(self, 
-        folder:htt.FoldersModelBuilder, 
-        resolution:int = 0.5, 
-        nodata:int = -9999,
-        overwrite:bool = False,
+    def __init__(
+        self, folder: htt.FoldersModelBuilder, resolution: int = 0.5, nodata: int = -9999, overwrite: bool = False
     ):
         self.folder = folder
         self.resolution = resolution
         self.nodata = nodata
         self.overwrite = overwrite
 
-
     def prepare_input(self):
         """
         Rasterize polder and watervlakken
         Create vrt of source dem and gxg with bounds of polder raster.
         """
-        #Rasterize polder polygon
+        # Rasterize polder polygon
         if not self.folder.dst.tmp.polder.exists():
             gdf_polder = gpd.read_file(self.folder.src.polder.path)
-            metadata = hrt.create_meta_from_gdf(gdf_polder, 
-                                                res=self.resolution)
+            metadata = hrt.create_meta_from_gdf(gdf_polder, res=self.resolution)
             gdf_polder["value"] = 1
             hrt.gdf_to_raster(
                 gdf=gdf_polder,
@@ -68,10 +68,10 @@ class ModelbuilderRasters:
                 raster_out=self.folder.dst.tmp.polder,
                 nodata=self.nodata,
                 metadata=metadata,
-                read_array=False
+                read_array=False,
             )
 
-        #Rasterize watergangen
+        # Rasterize watergangen
         if not self.folder.dst.tmp.watervlakken.exists():
             gdf = gpd.read_file(self.folder.src.watervlakken.path)
             metadata = self.folder.dst.tmp.polder.metadata
@@ -83,41 +83,41 @@ class ModelbuilderRasters:
                 nodata=self.nodata,
                 metadata=metadata,
                 read_array=False,
-                overwrite=False
+                overwrite=False,
             )
 
-        #Build vrt with correct extents of input rasters
+        # Build vrt with correct extents of input rasters
         for key in ["dem", "glg", "ggg", "ghg"]:
             output_file = getattr(self.folder.dst.tmp, key)
-            output_file.build_vrt(overwrite=False, 
-                            bounds=eval(self.folder.dst.tmp.polder.metadata.bbox), 
-                            input_files=getattr(self.folder.src, key), 
-                            resolution=0.5,
+            output_file.build_vrt(
+                overwrite=False,
+                bounds=eval(self.folder.dst.tmp.polder.metadata.bbox),
+                input_files=getattr(self.folder.src, key),
+                resolution=0.5,
             )
-
 
     def create_rasters(self):
         """Create output rasters dem and gxg"""
 
-        #Dem creation
+        # Dem creation
         def run_dem_window(block):
             """custom calc function on blocks in hrt.RasterCalculatorV2"""
-            block_out = block.blocks['dem']
+            block_out = block.blocks["dem"]
 
-            #Watervlakken ophogen naar +10mNAP
-            block_out[block.blocks['watervlakken']==1] = 10
+            # Watervlakken ophogen naar +10mNAP
+            block_out[block.blocks["watervlakken"] == 1] = 10
 
             block_out[block.masks_all] = self.nodata
             return block_out
-
 
         # init calculator
         self.dem_calc = hrt.RasterCalculatorV2(
             raster_out=folder.dst.dem,
             raster_paths_dict={
-                "dem" : folder.dst.tmp.dem,
-                "polder" : folder.dst.tmp.polder,
-                "watervlakken" : folder.dst.tmp.watervlakken,},
+                "dem": folder.dst.tmp.dem,
+                "polder": folder.dst.tmp.polder,
+                "watervlakken": folder.dst.tmp.watervlakken,
+            },
             nodata_keys=["polder"],
             mask_keys=["polder", "dem"],
             metadata_key="polder",
@@ -126,27 +126,26 @@ class ModelbuilderRasters:
             min_block_size=4096,
             verbose=True,
         )
-        #Run calculation of output raster
+        # Run calculation of output raster
         self.dem_calc.run(overwrite=True)
-    
 
-        #gxg raster creation
+        # gxg raster creation
         def run_gxg_window(block):
             """custom calc function on blocks in hrt.RasterCalculatorV2"""
-            block_out = block.blocks['gxg']
+            block_out = block.blocks["gxg"]
 
-            #Nodatamasks toepassen
+            # Nodatamasks toepassen
             block_out[block.masks_all] = self.nodata
             return block_out
-
 
         for gxg in ["glg", "ggg", "ghg"]:
             # init calculator
             gxg_calc = hrt.RasterCalculatorV2(
                 raster_out=getattr(self.folder.dst, gxg),
                 raster_paths_dict={
-                    "gxg" : getattr(self.folder.dst.tmp, gxg),
-                    "polder" : self.folder.dst.tmp.polder,},
+                    "gxg": getattr(self.folder.dst.tmp, gxg),
+                    "polder": self.folder.dst.tmp.polder,
+                },
                 nodata_keys=["polder"],
                 mask_keys=["polder", "gxg"],
                 metadata_key="polder",
@@ -155,8 +154,9 @@ class ModelbuilderRasters:
                 min_block_size=4096,
                 verbose=True,
             )
-            #Run calculation of output raster
+            # Run calculation of output raster
             gxg_calc.run(overwrite=False)
+
 
 
 
