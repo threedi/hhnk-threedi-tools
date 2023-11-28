84ece23ce342f0a5e6d08ad035bf34a8754bcc4d
wvangerwen <41104904+wvangerwen@users.noreply.github.com>
2023-11-28 16:17:11 +0100

    finish tests netcdf to gpkg

--- hhnk_threedi_tools/core/result_rasters/netcdf_to_gridgpkg.py
+++ hhnk_threedi_tools/core/result_rasters/netcdf_to_gridgpkg.py
@@ -23,6 +23,88 @@ htt.NetcdfToGPKG since v2023.5. Please rewrite your code."
 
 
 @dataclass
+class NetcdfTimeSeries:
+    """Timeseries contained in a netcdf"""
+
+    grid: hrt.ThreediResult  # type threedigrid.admin.gridresultadmin.GridH5ResultAdmin
+
+    def __post_init__(self):
+        self._wlvl_all = None
+        self._vol_all = None
+
+        self.timestamps = self.grid.nodes.timestamps
+
+    @property
+    def wlvl_2d_all(self):
+        if self._wlvl_all is None:
+            self._wlvl_all = self.get_timerseries(param="s1")
+        return self._wlvl_all
+
+    @property
+    def vol_2d_all(self):
+        if self._vol_all is None:
+            self._vol_all = self.get_timerseries(param="vol")
+        return self._vol_all
+
+    def get_timerseries(self, param):
+        """Get all timeseries for all 2d nodes.
+        slice(0,-1) doesnt retrieve the last timestep, using timestamp length instead.
+        """
+        return getattr(
+            self.grid.nodes.subset("2D_open_water").timeseries(indexes=slice(0, len(self.timestamps))), param
+        )
+
+    def get_waterlevel_and_volume(self, time_seconds: Union[int, str]) -> pd.DataFrame:
+        """Retrieve waterlevel and volume from netcdf
+
+        Parameters
+        ----------
+        time_seconds : Union[int,str]
+            time in seconds since start of calculation.
+            use "max" to get the max of all timesteps.
+        """
+        # Waterlevel and volume at all timesteps
+        if time_seconds == "max":
+            # Retrieve values when wlvl is max
+            max_index = self.wlvl_2d_all.argmax(axis=0)
+            wlvl = np.round([row[max_index[enum]] for enum, row in enumerate(self.wlvl_2d_all.T)], 5)
+            vol = np.round([row[max_index[enum]] for enum, row in enumerate(self.vol_2d_all.T)], 5)
+        else:
+            abs_diff = np.abs(self.timestamps - time_seconds)
+            idx = np.argmin(abs_diff)
+            if np.min(abs_diff) > 30:  # seconds diff.
+                raise ValueError(
+                    f"""Provided time_seconds {time_seconds} not found in netcdf timeseries.
+Closest timestep is {self.timestamps[idx]} seconds at index {idx}. \
+Debug by checking available timeseries through the (.ts).timeseries attributes"""
+                )
+
+            wlvl = np.round([row[idx] for row in self.wlvl_2d_all.T], 5)
+            vol = np.round([row[idx] for row in self.vol_2d_all.T], 5)
+        return pd.DataFrame([wlvl, vol]).T
+
+    def create_column_base(self, time_seconds):
+        """Return a base column name with hours and minutes.
+
+        Same input as self.get_waterlevel_volume.
+        """
+        # time_seconds = self.timestamps[np.argmin(np.abs(self.timestamps - time_seconds))]
+        if time_seconds == "max":
+            col_base = time_seconds
+        else:
+            timestep_h = time_seconds / 3600
+
+            if timestep_h % 1 == 0:  # round hours
+                col_base = f"{int(timestep_h)}h"
+            else:
+                if timestep_h < 1:
+                    col_base = f"{int(timestep_h*60)}min"
+                else:
+                    col_base = f"{int(np.floor(timestep_h))}h{int((timestep_h%1)*60)}min"
+        return col_base
+
+
+@dataclass
 class NetcdfToGPKG:
     """Transform netcdf into a gpkg. Can also correct waterlevels
     based on conditions. These are passed when running the function
@@ -51,9 +133,12 @@ class NetcdfToGPKG:
     panden_path: str = None
     panden_layer: str = None
 
+    def __post_init__(self):
+        self._ts = None
+
     @classmethod
     def from_folder(cls, folder: Folders, threedi_result: hrt.ThreediResult, **kwargs):
-        """Load from folder structure. Pand and waterdeel is taken from the folder structure."""
+        """Initialize from folder structure."""
         waterdeel_path = folder.source_data.damo
         waterdeel_layer = "Waterdeel"
 
@@ -78,6 +163,13 @@ class NetcdfToGPKG:
         """Default output if no path is specified."""
         return self.threedi_result.full_path("grid_wlvl.gpkg")
 
+    @property
+    def ts(self):
+        """Timeseries in the netcdf. Initialize once."""
+        if self._ts is None:
+            self._ts = NetcdfTimeSeries(grid=self.grid)
+        return self._ts
+
     def _calculate_layer_area_per_cell(
         self,
         grid_gdf: gpd.GeoDataFrame,
@@ -95,11 +187,10 @@ class NetcdfToGPKG:
         layer_name : str, optional, by default None
             Name of layer if the layer is part of a gpkg
         """
-
-        # Load layer as gdf
         gdf = None
+        # Load layer as gdf
         if (layer_path is not None) and (layer_path.exists()):
-            gdf = gpd.read_file(layer_path.path, layer=layer_name)
+            gdf = gpd.read_file(str(layer_path), layer=layer_name)
         else:
             print(f"Couldn't load {layer_path.name}. Ignoring it in correction.")
 
@@ -123,27 +214,147 @@ class NetcdfToGPKG:
             # Put in area in grid gdf and calculate percentage.
             grid_gdf_merged = grid_gdf.merge(overlay_df_grouped[area_col], left_on="id", right_on="id", how="left")
             grid_gdf_merged[perc_col] = grid_gdf_merged[area_col] / grid_gdf_merged.area * 100
-        return grid_gdf_merged[[area_col, perc_col]]
+            return grid_gdf_merged[[area_col, perc_col]]
+        return np.nan
 
-    def netcdf_to_grid_gpkg(
+    def create_base_gdf(self):
+        """Create  of grid"""
+        grid_gdf = gpd.GeoDataFrame()
+
+        # * inputs every element from row as a new function argument, creating a (square) box.
+        grid_gdf["geometry"] = [box(*row) for row in self.grid.nodes.subset("2D_ALL").cell_coords.T]
+        grid_gdf.crs = "EPSG:28992"
+
+        grid_gdf["id"] = self.grid.cells.subset("2D_open_water").id
+
+        grid_gdf = gpd.GeoDataFrame(grid_gdf, geometry="geometry")
+
+        return grid_gdf
+
+    def add_correction_parameters(
         self,
-        output_file=None,
+        grid_gdf: gpd.GeoDataFrame,
         replace_dem_below_perc: float = 50,
         replace_water_above_perc: float = 95,
         replace_pand_above_perc: float = 99,
-        overwrite=False,
+    ) -> gpd.GeoDataFrame:
+        """Determine which cells should have their waterlevel replaced by their neighbours.
+
+        Returns
+        -------
+        gpd.GeoDataFrame
+            extened grid_gdf with correction parameters columns.
+        """
+        # Percentage of dem in a calculation cell
+        # so we can make a selection of cells on model edge that need to be ignored
+        grid_gdf["dem_area"] = self.grid.cells.subset("2D_open_water").sumax
+        # Percentage dem in calculation cell
+        grid_gdf["dem_perc"] = grid_gdf["dem_area"] / grid_gdf.area * 100
+
+        grid_gdf[["water_area", "water_perc"]] = self._calculate_layer_area_per_cell(
+            grid_gdf=grid_gdf, layer_path=self.waterdeel_path, layer_name=self.waterdeel_layer
+        )
+        grid_gdf[["pand_area", "pand_perc"]] = self._calculate_layer_area_per_cell(
+            grid_gdf=grid_gdf, layer_path=self.panden_path, layer_name=self.panden_layer
+        )
+
+        # Select cells that need replacing of wlvl
+        grid_gdf["replace_dem"] = grid_gdf["dem_perc"] < replace_dem_below_perc
+        grid_gdf["replace_water"] = grid_gdf["water_perc"] > replace_water_above_perc
+        grid_gdf["replace_pand"] = grid_gdf["pand_perc"] > replace_pand_above_perc
+
+        # Write reason of replacing
+        grid_gdf["replace_all"] = False
+        grid_gdf.loc[grid_gdf["replace_dem"], "replace_all"] = "dem"
+        grid_gdf.loc[grid_gdf["replace_water"], "replace_all"] = "water"
+        grid_gdf.loc[grid_gdf["replace_pand"], "replace_all"] = "pand"
+
+        # Find neighbour cells and add their id's to a new column
+        neighbours = []
+        for row in grid_gdf.itertuples():
+            # find all indices that touch the cell
+            neighbours_ids = grid_gdf[grid_gdf.geometry.touches(row.geometry)].id.tolist()
+            # find the id of those indices
+            neighbours.append(str(neighbours_ids))
+        grid_gdf["neighbour_ids"] = neighbours
+        return grid_gdf
+
+    def get_waterlevels(self, grid_gdf, timesteps_seconds: list = None):
+        """Retrieve waterlevels at given timesteps"""
+        for timestep in timesteps_seconds:
+            # Make pretty column names
+            col_base = self.ts.create_column_base(time_seconds=timestep)
+            # Retrieve timeseries
+            grid_gdf[[f"wlvl_{col_base}", f"vol_{col_base}"]] = self.ts.get_waterlevel_and_volume(
+                time_seconds=timestep
+            )
+        return grid_gdf
+
+    def correct_waterlevels(self, grid_gdf, timesteps_seconds):
+        """Correct the waterlevel for the given timesteps. Results are only corrected
+        for cells where the 'replace_all' value is not False.
+
+        ----------
+        """
+        # create copy and index the id field so we can use the neighbours_ids column easily
+        grid_gdf_local = grid_gdf.copy()
+        grid_gdf_local.set_index("id", inplace=True)
+
+        for timestep in timesteps_seconds:
+            base_col = self.ts.create_column_base(time_seconds=timestep)
+            wlvl_col = f"wlvl_{base_col}"
+            wlvl_corr_col = f"{wlvl_col}_corr"
+            diff_col = f"diff_{base_col}"
+            idx_col = grid_gdf_local.columns.get_loc(wlvl_col) + 1
+
+            # Make copy of original wlvls and set to None when they need to be replaced
+            grid_gdf_local.insert(idx_col, wlvl_corr_col, grid_gdf_local[wlvl_col])
+            replace_idx = grid_gdf_local["replace_all"] != False  # noqa: E712
+            grid_gdf_local.loc[replace_idx, wlvl_corr_col] = None
+
+            # Loop cells that need replacing.
+            for row in grid_gdf_local.loc[replace_idx].itertuples():
+                # Calculate avg wlvl of neighbours
+
+                neighbour_ids = [int(i) for i in row.neighbour_ids[1:-1].split(",")]  # str list to list
+                neighbour_avg_wlvl = np.round(grid_gdf_local.loc[neighbour_ids][wlvl_corr_col].mean(), 5)
+
+                grid_gdf_local.loc[row.Index, wlvl_corr_col] = neighbour_avg_wlvl
+
+            grid_gdf_local.insert(idx_col + 1, diff_col, grid_gdf_local[wlvl_corr_col] - grid_gdf_local[wlvl_col])
+        return grid_gdf_local
+
+    def run(
+        self,
+        output_file=None,
+        timesteps_seconds: list[int, str] = ["max"],
+        replace_dem_below_perc: float = 50,
+        replace_water_above_perc: float = 95,
+        replace_pand_above_perc: float = 99,
+        wlvl_correction: bool = True,
+        overwrite: bool = False,
     ):
-        """Create gpkg of grid with maximum wlvl
+        """Transform netcdf into a grid gpkg.
 
-        output_file
-            When None is passed the output will be placed in the same directory as the netcdf
-
-        replace_dem_below_perc
+        Parameters
+        ----------
+        output_file, by default None
+            When None is passed the output will be placed in the same directory as the netcdf.
+            default name is: grid_wlvl.gpkg
+        timesteps_seconds, by default ["max"]
+            timesteps to make
+            options: int values in seconds
+                    "max" - maximum wlvl over calculation
+        replace_dem_below_perc : float, optional, by default 50
             if cell area has no dem (isna) above this value waterlevels will be replaced
-        replace_water_above_perc
+        replace_water_above_perc : float, optional, by default 95
             if cell has water surface area above this value waterlevels will be replaced
-        replace_pand_above_perc
+        replace_pand_above_perc : float, optional, by default 99
             if cell has pand surface area above this value waterlevels will be replaced
+        wlvl_correction : bool, optional, by default True
+            applies waterlevel correction when true.
+        overwrite : bool, optional, by default False
+            overwrite output if it exists
         """
 
         if output_file is None:
@@ -151,107 +362,20 @@ class NetcdfToGPKG:
 
         create = hrt.check_create_new_file(output_file=output_file, overwrite=overwrite)
         if create:
-            grid_gdf = gpd.GeoDataFrame()
+            grid_gdf = self.create_base_gdf()
 
-            # * inputs every element from row as a new function argument.
-            grid_gdf["geometry"] = [box(*row) for row in self.grid.nodes.subset("2D_ALL").cell_coords.T]
-            grid_gdf.crs = "EPSG:28992"
-            # nodes_2d["geometry"] = [Point(*row) for row in gr.nodes.subset("2D_ALL").coordinates.T] #centerpoints.
+            if wlvl_correction:
+                grid_gdf = self.add_correction_parameters(
+                    grid_gdf=grid_gdf,
+                    replace_dem_below_perc=replace_dem_below_perc,
+                    replace_water_above_perc=replace_water_above_perc,
+                    replace_pand_above_perc=replace_pand_above_perc,
+                )
 
-            grid_gdf["id"] = self.grid.cells.subset("2D_open_water").id
+            grid_gdf = self.get_waterlevels(grid_gdf=grid_gdf, timesteps_seconds=timesteps_seconds)
 
-            grid_gdf = gpd.GeoDataFrame(grid_gdf, geometry="geometry")
-
-            # Save to file
-            grid_gdf.to_file(output_file, driver="GPKG")
-
-            # Waterlevel and volume at all timesteps
-            s1_all = self.grid.nodes.subset("2D_open_water").timeseries(indexes=slice(0, -1)).s1
-            vol_all = self.grid.nodes.subset("2D_open_water").timeseries(indexes=slice(0, -1)).vol
-
-            # Retrieve values when wlvl is max
-            s1_max_index = s1_all.argmax(axis=0)
-            grid_gdf["wlvl_max_orig"] = np.round([row[s1_max_index[enum]] for enum, row in enumerate(s1_all.T)], 5)
-            grid_gdf["vol_m3_max_orig"] = np.round([row[s1_max_index[enum]] for enum, row in enumerate(vol_all.T)], 5)
-
-        if correct_wlvl:
-            # Percentage of dem in a calculation cell
-            # so we can make a selection of cells on model edge that need to be ignored
-            grid_gdf["dem_area"] = self.grid.cells.subset("2D_open_water").sumax
-            # Percentage dem in calculation cell
-            grid_gdf["dem_perc"] = grid_gdf["dem_area"] / grid_gdf.area * 100
-
-            grid_gdf["water_area", "water_perc"] = self._calculate_layer_area_per_cell(
-                grid_gdf=grid_gdf, layer_path=self.waterdeel_path, layer_name=self.waterdeel_layer
-            )
-            grid_gdf["pand_area", "pand_perc"] = self._calculate_layer_area_per_cell(
-                grid_gdf=grid_gdf, layer_path=self.panden_path, layer_name=self.panden_layer
-            )
-
-            # Select cells that need replacing of wlvl
-            grid_gdf["replace_dem"] = grid_gdf["dem_perc"] < replace_dem_below_perc
-            grid_gdf["replace_water"] = grid_gdf["water_perc"] > replace_water_above_perc
-            grid_gdf["replace_pand"] = grid_gdf["pand_perc"] > replace_pand_above_perc
-
-            # Write reason of replacing
-            grid_gdf["replace_all"] = 0
-            grid_gdf.loc[grid_gdf["replace_dem"] == True, "replace_all"] = "dem"
-            grid_gdf.loc[grid_gdf["replace_water"] == True, "replace_all"] = "water"
-            grid_gdf.loc[grid_gdf["replace_pand"] == True, "replace_all"] = "pand"
-
-            # TODO add option for user to pass cell ids to replace as well.
-
-    def waterlevel_correction(self, orig_col: str, corrected_col: str = None, gpkg_path=None, overwrite: bool = False):
-        """Correct the waterlevel for the input columns. Results are only corrected
-        for cells where the 'replace_all' value is not False.
-
-        This input needs to exist,
-            create it first with self.netcdf_to_grid_gpkg.
-
-        ----------
-        orig_col : str
-            Column with waterlevels that need to be replaced.
-        corrected_col : str, optional, by default None
-            Output column that will hold the corrected waterlevels. If this value is not passed
-            the name will be based on the original col, adding _corr at the end. If the original
-            column has _orig at the end it will replace that.
-        gpkg_path : path
-            if None, use the default value of grid_wlvl.gpkg. This function will add extra columns
-            and write to the same file as the input.
-        overwrite : bool, optional, by default False
-        """
-        if gpkg_path is None:
-            output_file = self.output_default
-
-        if create:
-            # Column names of output.
-            base_name = orig_col.split("_orig")[0]
-            diff_col = f"{base_name}_diff"
-            if corrected_col is None:
-                corrected_col = f"{base_name}_corr"
-
-            grid_gdf = gpkg_path.load()
-
-            grid_gdf[corrected_col] = grid_gdf[orig_col]
-            replace_idx = grid_gdf["replace_all"] != "0"
-            # set values to none so they are not used in calculation of new values.
-            grid_gdf.loc[replace_idx, corrected_col] = None
-
-            # Loop cells that need replacing.
-            for idx, row in grid_gdf.loc[replace_idx].iterrows():
-                # Find neighbour cells
-                neighbours_idx = grid_gdf[grid_gdf.geometry.touches(row.geometry)].index.tolist()
-                neighbours_id = [
-                    grid_gdf.loc[neighbour_idx].id for neighbour_idx in neighbours_idx if idx != neighbour_idx
-                ]
-                grid_gdf.loc[idx, "neighbours"] = str(neighbours_id)
-
-                # Calculate avg wlvl of neighbours
-                neighbour_avg_wlvl = np.round(grid_gdf.loc[neighbours_idx][corrected_col].mean(), 5)
-                grid_gdf.loc[idx, corrected_col] = neighbour_avg_wlvl
-
-            grid_gdf[diff_col] = grid_gdf[corrected_col] - grid_gdf[orig_col]
-
+            if wlvl_correction:
+                grid_gdf = self.correct_waterlevels(grid_gdf=grid_gdf, timesteps_seconds=timesteps_seconds)
             # Save to file
             grid_gdf.to_file(output_file, driver="GPKG")
 
@@ -264,11 +388,5 @@ if __name__ == "__main__":
 
     threedi_result = folder.threedi_results.one_d_two_d["katvoed #1 piek_ghg_T1000"]
 
-    self = ThreediGrid(folder=folder, threedi_result=threedi_result)
-    # self = ThreediGrid(threedi_result=threedi_result, waterdeel_path=folder.source_data.damo.path)
 
-    # #Convert netcdf to grid gpkg
-    self.netcdf_to_grid_gpkg()
-
-    # #Replace waterlevel of selected cells with avg of neighbours.
-    # self.waterlevel_correction(output_col="wlvl_max_replaced")
+# %%
--- tests/test_netcdf_to_gridgpkg.py
+++ tests/test_netcdf_to_gridgpkg.py
@@ -1,14 +1,18 @@
 # %%
 
-"""Functional testing for oneDtwoD object"""
+"""Functional testing creating a grid.gpkg from a .nc"""
 import os
 import pathlib
+from dataclasses import dataclass
+from multiprocessing import Value
 
+import hhnk_research_tools as hrt
 import pytest
+from flask import g
 
 from hhnk_threedi_tools.core.checks.one_d_two_d import OneDTwoDTest
 from hhnk_threedi_tools.core.result_rasters import netcdf_to_gridgpkg
-from tests.config import FOLDER_NEW, FOLDER_TEST
+from tests.config import FOLDER_TEST, TEMP_DIR
 
 if __name__ == "__main__":
     import importlib
@@ -17,61 +21,39 @@ if __name__ == "__main__":
 
 
 def test_netcdf_to_gridgpkg():
-    pass
+    # input
+    timesteps_seconds = ["max", 5400]
+
+    netcdf_gpkg = netcdf_to_gridgpkg.NetcdfToGPKG.from_folder(
+        folder=FOLDER_TEST,
+        threedi_result=FOLDER_TEST.threedi_results.batch["batch_test"].downloads.piek_glg_T10.netcdf,
+    )
+
+    grid_gdf = netcdf_gpkg.create_base_gdf()
+
+    grid_gdf = netcdf_gpkg.add_correction_parameters(
+        grid_gdf=grid_gdf,
+        replace_dem_below_perc=50,
+        replace_water_above_perc=95,
+        replace_pand_above_perc=95,
+    )
+
+    with pytest.raises(ValueError):
+        grid_gdf = netcdf_gpkg.get_waterlevels(grid_gdf=grid_gdf, timesteps_seconds=[7250])
+
+    # get and correct waterlevels
+    grid_gdf = netcdf_gpkg.get_waterlevels(grid_gdf=grid_gdf, timesteps_seconds=timesteps_seconds)
+    grid_gdf = netcdf_gpkg.correct_waterlevels(grid_gdf=grid_gdf, timesteps_seconds=timesteps_seconds)
+
+    assert np.round(grid_gdf.loc[1, "wlvl_1h30min"], 5) == 1.60609
+    assert np.round(grid_gdf.loc[1, "wlvl_1h30min_corr"], 5) == 0.66457
+
+    # Test run statement
+    output_file = TEMP_DIR.joinpath(f"grid_wlvl_{hrt.current_time('%H%M%S')}.gpkg")
+    netcdf_gpkg.run(output_file=output_file)
+
+    assert output_file.exists()
 
 
 if __name__ == "__main__":
     test_netcdf_to_gridgpkg()
-
-# %%
-self = netcdf_gpkg = netcdf_to_gridgpkg.NetcdfToGPKG.from_folder(
-    folder=FOLDER_TEST, threedi_result=FOLDER_TEST.threedi_results.batch["batch_test"].downloads.piek_glg_T10.netcdf
-)
-
-
-import geopandas as gpd
-import numpy as np
-from shapely import box
-
-layer_path = netcdf_gpkg.waterdeel_path
-layer_name = netcdf_gpkg.waterdeel_layer
-column_base_name = "water"
-
-
-
-# %%
-if True:
-            grid_gdf = gpd.GeoDataFrame()
-
-            # Waterlevel and volume at all timesteps
-            s1_all = self.grid.nodes.subset("2D_open_water").timeseries(indexes=slice(0, -1)).s1
-            vol_all = self.grid.nodes.subset("2D_open_water").timeseries(indexes=slice(0, -1)).vol
-
-            # * inputs every element from row as a new function argument.
-            grid_gdf["geometry"] = [box(*row) for row in self.grid.nodes.subset("2D_ALL").cell_coords.T]
-            grid_gdf.crs = "EPSG:28992"
-            # nodes_2d["geometry"] = [Point(*row) for row in gr.nodes.subset("2D_ALL").coordinates.T] #centerpoints.
-
-            grid_gdf["id"] = self.grid.cells.subset("2D_open_water").id
-
-            # Retrieve values when wlvl is max
-            s1_max_index = s1_all.argmax(axis=0)
-            grid_gdf["wlvl_max_orig"] = np.round([row[s1_max_index[enum]] for enum, row in enumerate(s1_all.T)], 5)
-            grid_gdf["vol_m3_max_orig"] = np.round([row[s1_max_index[enum]] for enum, row in enumerate(vol_all.T)], 5)
-
-            # Percentage of dem in a calculation cell
-            # so we can make a selection of cells on model edge that need to be ignored
-            grid_gdf["dem_area"] = self.grid.cells.subset("2D_open_water").sumax
-            # Percentage dem in calculation cell
-            grid_gdf["dem_perc"] = grid_gdf["dem_area"] / grid_gdf.area * 100
-
-            grid_gdf["water_area", "water_perc"] = self.calculate_layer_area_per_cell(
-                grid_gdf=grid_gdf,
-                layer_path=self.waterdeel_path,
-                layer_name=self.waterdeel_layer,
-            )
-# %%
-
-if True:
-    # Load layer as gdf
-
