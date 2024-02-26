5c7170d1809c17593f9318de183e37b43ea83a28
Gerwen <41104904+wvangerwen@users.noreply.github.com>
2024-02-26 11:34:47 +0100

    update docstring

--- hhnk_threedi_tools/core/raster_creation/storage_lookup.py
+++ hhnk_threedi_tools/core/raster_creation/storage_lookup.py
@@ -7,7 +7,29 @@ import hhnk_research_tools as hrt
 
 
 def create_storage_lookup(rootzone_thickness_cm, storage_unsa_sim_path=None) -> pd.DataFrame:
-    """function creates a list of available storage for given rootzone thickness"""
+    """function creates a list of available storage for given rootzone thickness
+    
+    The unsa_sim.inp is a table that contains storage per soil type for different dewathering depths and depths of the root zone. It gives the amount of storage available in the unsaturated zone, which is a non-linear relation to the dewathering depth.
+    See https://content.oss.deltares.nl/delft3d/manuals/SOBEK_User_Manual.pdf p817
+    Assumption 1: Sand is used for raising project areas, which is soil type 14: Podzolic, coarse textured sandy soil: sandy soil (gHd30).
+    Assumption 2: Project areas left unpaved are mainly grown with grass, which according to ROOT_SIM.INP translates to a root zone thickness of 0.2 m.
+    Example: UNSA_SIM.INP
+    column 1 = soil type
+    column 2 = rootzone thickness (cm)
+    column 3 = groundwater level (meter below surface)
+    column 4 = root zone soil moisture storage in equilibrium conditions (mm)
+    column 5 = potential capillary rise (mm/day)
+    column 6 = storage coefficient (m/m)
+    Column 6 gives the local storage in the unsaturated zone, not the total! So this value must be integrated over the entire depth of the soil. Also, the rooth zone is a seperate reservoir. The difference in storage in the local and maximum rooth zone (column 4) must be added to the storage in the unsaturated zone to find the total storage.
+    Storage at depth d 
+    = 
+    Integrated over d of storage coefficient 
+    + 
+    storage in the rooth zone at depth = 0 
+    - 
+    storage at the roost zone at depth d
+    
+    """
 
     if storage_unsa_sim_path is None:
         storage_unsa_sim_path = hrt.get_pkg_resource_path(package_resource=htt.resources, name="unsa_sim.csv")
@@ -51,7 +73,7 @@ def create_storage_lookup(rootzone_thickness_cm, storage_unsa_sim_path=None) -> 
                 local_storage_sum = 0
             else:
                 local_coef = np.interp(x=d, xp=xlist, fp=sclist)
-                local_storage = local_coef * dstep * 1000
+                local_storage = local_coef * dstep * 1000 # converted to mm as rooth zone storage is too
                 local_storage_sum = local_storage_sum + local_storage
 
             # calculate storage in rooth zone
@@ -59,7 +81,7 @@ def create_storage_lookup(rootzone_thickness_cm, storage_unsa_sim_path=None) -> 
             avail_rooth_storage = max_soil_rooth_storage - rooth_storage
 
             # Total availeble storage
-            total_storage = (avail_rooth_storage + local_storage_sum) / 1000
+            total_storage = (avail_rooth_storage + local_storage_sum) / 1000 # back to meters
 
             storage_lookup_dict.append(
                 {
