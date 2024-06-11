591fd3c4f4259f7346e17851e01b23de9730fe61
Wietse van Gerwen <41104904+wvangerwen@users.noreply.github.com>
2024-06-11 12:15:59 +0200

    rename approve_ok to approx_ok

--- hhnk_threedi_tools/core/climate_scenarios/klimaatsommen_prep.py
+++ hhnk_threedi_tools/core/climate_scenarios/klimaatsommen_prep.py
@@ -272,7 +272,7 @@ class KlimaatsommenPrep:
         """Raster statistics for single scenario"""
         raster = getattr(scenario, raster_type)
 
-        stats = raster.statistics(approve_ok=True, force=True)
+        stats = raster.statistics(approx_ok=True, force=True)
 
         # Fill row data
         info_row = pd.Series(dtype=object)
