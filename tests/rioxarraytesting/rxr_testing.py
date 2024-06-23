1ffb042fe798d7e2634aaa4f1056fea504f23908
wvangerwen <41104904+wvangerwen@users.noreply.github.com>
2024-06-23 12:53:02 +0200

    speed tests home pc
    Made some changes afterwards. rxr no blocks seems to be the prefered route.

--- tests/rioxarraytesting/rxr_testing.py
+++ tests/rioxarraytesting/rxr_testing.py
@@ -11,6 +11,7 @@ from pathlib import Path
 import geopandas as gpd
 import hhnk_research_tools as hrt
 import numpy as np
+import pandas as pd
 import rasterio as rio
 import rioxarray as rxr
 import xarray as xr
@@ -19,20 +20,47 @@ from hhnk_threedi_tools.core.result_rasters.calculate_raster import BaseCalculat
 from tests.config import FOLDER_TEST, TEMP_DIR
 
 CHUNKSIZE = 4096
-SETUP = "full"
+# CHUNKSIZE = 8192
+# CHUNKSIZE = 16384
+# CHUNKSIZE = 1024
+
+SETUP = "clip_small"
+
+
 SETUPS = {
     "clip_small": {"statistics": {"min": -32767.0, "max": 32765.0, "mean": 670.35384, "std": 2858.262956}},
     "clip_medium": {"statistics": {"min": -32767.0, "max": 32765.0, "mean": 664.976118, "std": 2841.971657}},
     "full": {"statistics": {"min": -32760.0, "max": 32765.0, "mean": 699.288424, "std": 2826.983867}},
 }
 
+# When using chunksize 16384, to make wlvl rater, stats come out a bit different.
+SETUPS = {
+    "clip_small": {"statistics": {"min": -32767.0, "max": 32765.0, "mean": 670.353979, "std": 2858.263808}},
+    "clip_medium": {"statistics": {"min": -32767.0, "max": 32765.0, "mean": 664.976254, "std": 2841.972502}},
+    "full": {"statistics": {"min": -32767.0, "max": 32765.0, "mean": 664.976254, "std": 2841.972502}},
+}
+
 
 PLAYGROUND_DIR = Path(r"d:\projecten\D2402.HHNK.Ondersteuning_Python\03.playground")
+PLAYGROUND_DIR = Path(r"C:\Users\Wietse\Documents\HHNK\playground")
 # dem = FOLDER_TEST.model.schema_base.rasters.dem FIXME: Exception: No connection or database path provided
 dem = hrt.Raster(PLAYGROUND_DIR / SETUP / "dem.tif")
 grid_gdf = gpd.read_file(PLAYGROUND_DIR / SETUP / "grid_corr.gpkg")
 wlvl = hrt.Raster(PLAYGROUND_DIR / SETUP / "wlvl.tif")
 
+# Performance
+perf = pd.DataFrame()
+perf["wvg_home_small_1024"] = [28.03, 1.87, 1.95, 4.44]
+perf["wvg_home_small_4096"] = [28.66, 2.36, 1.83, 3.42]
+perf["wvg_home_small_16384"] = [28.45, 2.65, 1.9, 3.32]
+perf["wvg_home_medium_1024"] = [0, 9.2, 10.2, 18.2]
+perf["wvg_home_medium_4096"] = [97.36, 8.1, 9.2, 11.7]
+perf["wvg_home_medium_16384"] = [104.1, 8.39, 8.03, 12.0]
+perf["wvg_home_full_4096"] = [343.7, 22.5, 26.8, 56.9]
+perf["wvg_home_full_16384"] = [0, 21.4, 30.17, 47.1]
+perf = perf.T
+perf.columns = ["wlvl", "rxr", "rxr_block", "hrt"]
+perf
 
 # %% water level
 
@@ -45,10 +73,12 @@ if not wlvl.exists():
         "wlvl_column": "wlvl_max_replaced",
     }
     with BaseCalculatorGPKG(**calculator_kwargs) as basecalc:
-        basecalc.run(output_file=wlvl.path, mode="MODE_WLVL", overwrite=True)
+        basecalc.run(output_file=wlvl.path, mode="MODE_WLVL", overwrite=True, min_block_size=CHUNKSIZE)
     print(time.time() - now)
 
-depth = hrt.Raster(PLAYGROUND_DIR / SETUP / "depth.tif")
+depth_rxr = hrt.Raster(PLAYGROUND_DIR / SETUP / "depth_rxr.tif")
+depth_rxr_block = hrt.Raster(PLAYGROUND_DIR / SETUP / "depth_rxr_blocks.tif")
+depth_hrt = hrt.Raster(PLAYGROUND_DIR / SETUP / "depth_hrt.tif")
 
 
 def get_rasters(chunksize=CHUNKSIZE):
@@ -62,20 +92,30 @@ def get_rasters(chunksize=CHUNKSIZE):
     return raster1, raster2
 
 
-def write_to_raster(result, scale_factor):
+def write_to_raster(raster_out, result, scale_factor):
+    dtype = "int16"
+    if "float" in dtype:
+        compress = "LERC_DEFLATE"
+    else:
+        compress = "ZSTD"
+
     result.rio.to_raster(
-        depth.base,
-        chunks={"x": CHUNKSIZE, "y": CHUNKSIZE},
-        # lock=True, #Use dask multithread
-        compress="ZSTD",
-        tiled=True,
-        PREDICTOR=2,
-        ZSTD_LEVEL=1,
-        dtype="int16",
+        raster_out.base,
+        # chunks={"x": CHUNKSIZE, "y": CHUNKSIZE},
+        chunks=True,
+        # lock=threading.Lock(), #Use dask multithread
+        lock=True,
+        tiled=1024,
+        windowed=True,
+        COMPRESS=compress,  # gdal options
+        PREDICTOR=2,  # gdal options
+        ZSTD_LEVEL=1,  # gdal options
+        NUM_THREADS="ALL_CPUS",  # gdal options
+        dtype=dtype,
     )
 
     # Settings the scale_factor does not work with rioxarray. T
-    with rio.open(depth.base, "r+") as src:
+    with rio.open(raster_out.base, "r+") as src:
         src.scales = (scale_factor,)
 
 
@@ -96,7 +136,7 @@ def rxr_calc_scaled_raster():
     raster1, raster2 = get_rasters()
 
     # Subtract the rasters, excluding NoData values
-    result = raster1 - raster2
+    result = raster2 - raster1
 
     # Assign values less than 0 to NoData
 
@@ -106,14 +146,14 @@ def rxr_calc_scaled_raster():
 
     # Export the result directly to the output raster in chunks
     result.rio.set_nodata(raster1.rio.nodata)
-    write_to_raster(result, scale_factor=0.001)
+    write_to_raster(depth_rxr, result, scale_factor=0.001)
 
     print(time.time() - now)
 
 
 rxr_calc_scaled_raster()
 
-assert depth.statistics(approve_ok=False) == SETUPS[SETUP]["statistics"]
+assert depth_rxr.statistics(approve_ok=False) == SETUPS[SETUP]["statistics"]
 
 
 # %%
@@ -123,17 +163,17 @@ def rxr_map_blocks():
 
     raster1, raster2 = get_rasters(chunksize=CHUNKSIZE)
 
-    result = xr.zeros_like(raster1)
+    result = xr.full_like(raster1, raster1.rio.nodata)
 
-    def calc(da1, da2):
+    def calc(ra1, da1, da2):
         # if da1.x.data[0] != 10958122:
-        #     return xr.zeros_like(da1)
+        # return ra1
 
         # if da1.x.data[0] != 109581:
         #     return xr.zeros_like(da1)
         # print(da2)
         # else:
-        r = da1 - da2
+        r = da2 - da1
 
         # print(da)
         # print(da.x.data[0])
@@ -145,18 +185,18 @@ def rxr_map_blocks():
         return r
 
     # result += 2
-    result = xr.map_blocks(calc, obj=raster1, args=[raster2], template=result)
+    result = xr.map_blocks(calc, obj=result, args=[raster1, raster2], template=result)
 
     result.rio.set_nodata(raster1.rio.nodata)
 
-    write_to_raster(result, scale_factor=0.001)
+    write_to_raster(depth_rxr_block, result, scale_factor=0.001)
 
     print(time.time() - now)
 
 
 rxr_map_blocks()
 
-assert depth.statistics(approve_ok=False) == SETUPS[SETUP]["statistics"]
+assert depth_rxr_block.statistics(approve_ok=False) == SETUPS[SETUP]["statistics"]
 # %%
 
 
@@ -167,7 +207,7 @@ def hrt_rastercalculator():
     def run_rtype_window(block):
         """Custom calc function on blocks in hrt.RasterCalculatorV2"""
         block_out = block.blocks["wlvl"] - block.blocks["dem"]
-        block_out *= 1000
+        # block_out *= 1000
 
         # Nodatamasks toepassen
         block_out[block.masks_all] = dem.nodata
@@ -176,7 +216,7 @@ def hrt_rastercalculator():
 
     # Calculate drooglegging raster
     raster_calc = hrt.RasterCalculatorV2(
-        raster_out=depth,
+        raster_out=depth_hrt,
         raster_paths_dict={
             "dem": dem,
             "wlvl": wlvl,
@@ -198,6 +238,6 @@ def hrt_rastercalculator():
 
 hrt_rastercalculator()
 
-assert depth.statistics(approve_ok=False) == SETUPS[SETUP]["statistics"]
+assert depth_hrt.statistics(approve_ok=False) == SETUPS[SETUP]["statistics"]
 
 # %%
