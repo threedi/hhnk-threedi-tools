de4a49f55a10ad77c4f3873606c8dd6c3e5ea5b5
wietsevangerwen <41104904+wvangerwen@users.noreply.github.com>
2024-11-26 18:37:25 +0100

    refactor GridToWaterLevel test

--- tests/test_result_rasters.py
+++ tests/test_result_rasters.py
@@ -3,51 +3,35 @@ import geopandas as gpd
 import hhnk_research_tools as hrt
 
 # pd.options.mode.chained_assignment = 'raise' #catch SettingWithCopyWarning
-import pytest
-
-from hhnk_threedi_tools.core.result_rasters.grid_to_raster import GridToRaster
+from hhnk_threedi_tools.core.result_rasters.grid_to_raster import GridToWaterDepth, GridToWaterLevel
 from tests.config import FOLDER_TEST, TEMP_DIR, TEST_DIRECTORY
 
 TEST_RESULT_DIR = TEST_DIRECTORY / r"test_result_rasters"
 
 
-class TestGridToRaster:
-    @pytest.fixture(scope="class")
-    def grid_gdf(self):
-        return gpd.read_file(TEST_RESULT_DIR / "grid_corr_tiny.gpkg", driver="GPKG")
+def test_grid_to_raster():
+    grid_gdf = gpd.read_file(TEST_RESULT_DIR / "grid_corr_tiny.gpkg", engine="pyogrio")
+    dem_path = TEST_RESULT_DIR / "dem_tiny.tif"
+    wlvl_column = "wlvl_max_replaced"
 
-    @pytest.fixture(scope="class")
-    def basecalc(self, grid_gdf):
-        calculator_kwargs = {
-            "dem_path": TEST_RESULT_DIR / "dem_tiny.tif",
-            "grid_gdf": grid_gdf,
-            "wlvl_column": "wlvl_max_replaced",
-        }
-        with GridToRaster(**calculator_kwargs) as basecalc:
-            return basecalc
+    # WLVL raster
+    wlvl_raster = hrt.Folder(TEMP_DIR).full_path(f"wlvl_corr_{hrt.get_uuid()}.tif")
 
-    def test_wlvl(self, basecalc):
-        output_file = hrt.Folder(TEMP_DIR).full_path(f"wlvl_corr_{hrt.get_uuid()}.tif")
-        basecalc.run(output_file=output_file.path, mode="MODE_WLVL", overwrite=True)
+    gridtowlvl = GridToWaterLevel(dem_path=dem_path, grid_gdf=grid_gdf, wlvl_column=wlvl_column)
+    gridtowlvl.run(output_file=wlvl_raster, overwrite=True)
 
-        assert output_file.sum() == 43.918495178222656
+    assert wlvl_raster.sum() == 43.918495178222656
 
-    def test_wdepth(self, basecalc):
-        output_file = hrt.Folder(TEMP_DIR).full_path(f"wdepth_corr_{hrt.get_uuid()}.tif")
-        basecalc.run(output_file=output_file.path, mode="MODE_WDEPTH", overwrite=True)
-        assert output_file.sum() == 5.868329048156738
+    # WDEPTH raster
+    wdepth_raster = hrt.Folder(TEMP_DIR).full_path(f"wdepth_corr_{hrt.get_uuid()}.tif")
+
+    gridtowdepth = GridToWaterDepth(dem_path=dem_path, wlvl_path=wlvl_raster)
+    gridtowdepth.run(output_file=wdepth_raster, overwrite=True)
+
+    assert wdepth_raster.sum() == 5.868329048156738
 
 
 # %%
 if __name__ == "__main__":
-    import inspect
-
-    selftest = TestGridToRaster()
-    basecalc = selftest.basecalc(selftest.grid_gdf())
-    # self = selftest.bl_test
-    # Run all testfunctions
-    for i in dir(selftest):
-        if i.startswith("test_") and hasattr(inspect.getattr_static(selftest, i), "__call__"):
-            print(i)
-            getattr(selftest, i)(basecalc)
+    test_grid_to_raster()
 # %%
