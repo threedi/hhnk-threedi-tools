48474df33ed4b07e665f23054f17e895cedb6e99
Esther van der Laan <esthervdl5@gmail.com>
2024-12-05 13:02:06 +0100

    update damo exporter

--- hhnk_threedi_tools/core/schematisation_builder/DAMO_exporter.py
+++ hhnk_threedi_tools/core/schematisation_builder/DAMO_exporter.py
@@ -1,29 +1,16 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Nov 29 09:17:01 2024
-
-@author: esther.vanderlaan
-"""
-# %%
+"""DAMO exporter based on model extent"""
 import geopandas as gpd
+from shapely.geometry import box
 import pytest
 from local_settings import DATABASES
-# %%
 from hhnk_research_tools.sql_functions import (
     database_to_gdf,
     sql_builder_select_by_location,
 )
 
-# Temporary: hard coded model extent selection
-POLDERS =  gpd.read_file(r'E:\01.basisgegevens\Polders\polderclusters.gpkg', engine="pyogrio")
-model_extent = POLDERS['geometry'][54] # this polder, because others are too large
-#polygon_wkt = POLDERS['geometry'][54]
-
-EPSG_CODE = "28992"
-
-
-def DAMO_exporter(model_extent, table_name="HYDROOBJECT"):
-
+def DAMO_exporter(model_extent, table_name="HYDROOBJECT", EPSG_CODE = "28992"):
+    # if instead of a model_extent, polder_name is given as input.
+    # make it possible to select geometry of polder from polderclusters.gpkg
     db_dicts = {
         "aquaprd": DATABASES.get("aquaprd_lezen", None),
         "bgt": DATABASES.get("bgt_lezen", None),
@@ -31,21 +18,26 @@ def DAMO_exporter(model_extent, table_name="HYDROOBJECT"):
     
     schema = "DAMO_W"
     
-    # Load area for selection
-    model_extent_gdf = gpd.read_file(model_extent, engine="pyogrio")
-    polygon_wkt = model_extent_gdf.iloc[0]["geometry"]
-    
+    # make bbox --> to simply string request to DAMO db
+    min_x, min_y, max_x, max_y = model_extent.bounds
+    bbox_model = box(min_x, min_y, max_x, max_y)
+
     # Build sql to select by input polygon
     sql = sql_builder_select_by_location(
-        schema=schema, table_name=table_name, epsg_code=EPSG_CODE, polygon_wkt=polygon_wkt, simplify=True
+        schema=schema, table_name=table_name, epsg_code=EPSG_CODE, polygon_wkt=bbox_model, simplify=True
     )
     db_dict = db_dicts["aquaprd"]
     columns = None
-    
-    
-    gdf, sql2 = database_to_gdf(db_dict=db_dict, sql=sql, columns=columns)
+        
+    bbox_gdf, sql2 = database_to_gdf(db_dict=db_dict, sql=sql, columns=columns)
 
-    return gdf
-# %%
+    # clip gdf to model_extent again
+    gdf_model = gpd.clip(bbox_gdf, model_extent)
 
-# afhankelijk van hoe dit gebruikt gaat worden, __main__ toevoegen om functie direct met dit script te runnen
+    return gdf_model
+
+#Test
+POLDERS_PATH = r'E:\01.basisgegevens\Polders\polderclusters.gpkg'
+POLDERS =  gpd.read_file(POLDERS_PATH, engine="pyogrio")
+model_extent = POLDERS['geometry'][15] 
+output_DAMO = DAMO_exporter(model_extent)
