d43d69c610757269f463f9ad2e5a7bcfca6775a2
Overmeen <84539501+Overmeen@users.noreply.github.com>
2024-12-06 12:37:49 +0100

    Update project.py

--- hhnk_threedi_tools/core/project.py
+++ hhnk_threedi_tools/core/project.py
@@ -1,4 +1,4 @@
-from folders import Folders
+from hhnk_threedi_tools.core.folders import Folders
 import json
 from pathlib import Path
 import time
@@ -9,16 +9,17 @@ class Project:
         self.folder = folder # set project folder
 
         if Path(folder).exists():
-            self.folders = Folders(folder, create=False) # set folders instance for existing project folder
+            folders = Folders(folder, create=False) # set folders instance for existing project folder
         else:
             Path(folder).mkdir(parents=True, exist_ok=True)
-            self.folders = Folders(folder, create=True) # create folders instance for new project folder
+            folders = Folders(folder, create=True) # create folders instance for new project folder
 
-        if self.folders.project_json.exists():
-            self.load_from_json() # load variables from json if exists (fixed filename)
+        self.json_path = str(folders.project_json.path)
+        if folders.project_json.exists():
+            self.load_from_json(self.json_path) # load variables from json if exists (fixed filename)
         else: 
             self.initialise_new_project() # initialise new project
-            self.save_to_json()
+            self.save_to_json(self.json_path)
 
     def initialise_new_project(self):
         """ Create all Project variables for a new project """
@@ -28,18 +29,18 @@ class Project:
 
     def update_project_status(self, status):
         self.project_status = status
-        self.save_to_json()
+        self.save_to_json(self.json_path)
 
     def retrieve_project_status(self):
         return self.project_status
 
-    def save_to_json(self):
+    def save_to_json(self, filepath):
         self.project_date = str(time.strftime("%Y-%m-%d %H:%M:%S")) # update project date
-        with open(self.folders.project_json.path, 'w') as f:
+        with open(filepath, 'w') as f:
             json.dump(self.__dict__, f)
 
-    def load_from_json(self):
-        with open(self.folders.project_json.path, 'r') as f:
+    def load_from_json(self, filepath):
+        with open(filepath, 'r') as f:
             data = json.load(f)
             self.__dict__.update(data)
 
