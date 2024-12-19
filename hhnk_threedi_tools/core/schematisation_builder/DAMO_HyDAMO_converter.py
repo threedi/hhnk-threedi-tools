20b127b771378b32e14db102dbe36d037cbf7f0d
Overmeen <84539501+Overmeen@users.noreply.github.com>
2024-12-19 15:07:33 +0100

    Ruff formatting and move sources to resources

--- hhnk_threedi_tools/core/schematisation_builder/DAMO_HyDAMO_converter.py
+++ hhnk_threedi_tools/core/schematisation_builder/DAMO_HyDAMO_converter.py
@@ -1,10 +1,12 @@
-from pathlib import Path
-import pandas as pd
-import geopandas as gpd
-import xml.etree.ElementTree as ET
 import json
+import xml.etree.ElementTree as ET
+from pathlib import Path
 
-WATERSCHAPSCODE = 12 # Hoogheemraadschap Hollands Noorderkwartier
+import geopandas as gpd
+import pandas as pd
+
+WATERSCHAPSCODE = 12  # Hoogheemraadschap Hollands Noorderkwartier
+
 
 class Converter:
     """
@@ -28,6 +30,11 @@ class Converter:
         Path to the DAMO schema (xml file)
     layers : list
         List of layer names to convert to HyDAMO
+
+    Sources
+    ----------
+    HyDAMO schema: https://github.com/HetWaterschapshuis/HyDAMOValidatieModule/blob/main/hydamo_validation/schemas/hydamo/HyDAMO_2.3.json
+    DAMO_schema: XML file retrieved from Het Waterschapshuis by mail
     """
 
     def __init__(self, DAMO_path, HyDAMO_path, layers, hydamo_schema_path=None, damo_schema_path=None):
@@ -36,12 +43,12 @@ class Converter:
         self.layers = layers
 
         if hydamo_schema_path is None:
-            self.hydamo_schema_path = Path(__file__).parent / "HyDAMO_2_3/HyDAMO_2.3.json"
+            self.hydamo_schema_path = Path(__file__).parents[2] / "resources/HyDAMO_2_3/HyDAMO_2_3.json"
         else:
             self.hydamo_schema_path = Path(hydamo_schema_path)
-        
+
         if damo_schema_path is None:
-            self.damo_schema_path = Path(__file__).parent / "DAMO_2_3/DAMO_23.xml"
+            self.damo_schema_path = Path(__file__).parents[2] / "resources/DAMO_2_3/DAMO_2_3.xml"
         else:
             self.damo_schema_path = Path(damo_schema_path)
 
@@ -59,7 +66,7 @@ class Converter:
         -------
         """
         # Read XML file content
-        with open(self.damo_schema_path, 'r') as xml_file:
+        with open(self.damo_schema_path, "r") as xml_file:
             xml_text = xml_file.read()
 
         # Parse the XML text
@@ -95,7 +102,7 @@ class Converter:
         for data_element_element in data_element_elements:
             data_name = data_element_element.find(".//Name").text
             field_dict = {}
-            
+
             fields_element = data_element_element.find(".//Fields")
             if fields_element is not None:
                 field_array_element = fields_element.find(".//FieldArray")
@@ -109,18 +116,18 @@ class Converter:
                             field_type = field_element.find("Type").text
                             field_type = field_type.replace("esriFieldType", "")
                         field_dict[field_name] = field_type
-                        
+
             self.objects[data_name] = field_dict
 
     def retrieve_HyDAMO_definitions(self):
         """
         Reads JSON definitions and assigns them as instance variables.
         """
-        with open(self.hydamo_schema_path, 'r') as json_file:
+        with open(self.hydamo_schema_path, "r") as json_file:
             hydamo_schema = json.load(json_file)
-        
+
         definitions = hydamo_schema.get("definitions", {})
-        
+
         for key, value in definitions.items():
             attr_name = f"definition_{key}"
             setattr(self, attr_name, value)
@@ -134,7 +141,7 @@ class Converter:
         """
         for layer_name in self.layers:
             self.convert_layer(layer_name)
-        
+
     def convert_layer(self, layer_name):
         """
         Convert the layer to HyDAMO and write to the target HyDAMO geopackage.
@@ -143,14 +150,14 @@ class Converter:
         ----------
         layer : str
             Name of the layer to convert
-        
+
         Returns
         ----
         """
         layer_gdf = gpd.read_file(self.DAMO_path, layer=layer_name)
         layer_gdf = self.convert_attributes(layer_gdf, layer_name)
         layer_gdf = self.add_column_NEN3610id(layer_gdf, layer_name)
-        layer_gdf.to_file(self.HyDAMO_path, layer=layer_name, driver='GPKG')
+        layer_gdf.to_file(self.HyDAMO_path, layer=layer_name, driver="GPKG")
 
     def convert_attributes(self, layer_gdf, layer_name):
         """
@@ -170,7 +177,7 @@ class Converter:
             column_name = column_name.lower()
             layer_gdf[column_name] = self.convert_column(layer_gdf[column_name], column_name, layer_name)
         return layer_gdf
-    
+
     def convert_column(self, column, column_name, layer_name):
         """
         Convert the attribute column to HyDAMO.
@@ -194,7 +201,7 @@ class Converter:
         # Convert the field type to the correct type
         column = self.convert_field_type(column, field_type)
         return column
-    
+
     def get_field_type(self, column_name, layer_name):
         """
         Retrieves the field type of a specific attribute in a definition.
@@ -208,10 +215,10 @@ class Converter:
         """
         # Find the object definition
         object_def = getattr(self, f"definition_{layer_name}", {})
-        
+
         # Navigate to the 'properties' of the object
         properties = object_def.get("properties", {})
-        
+
         # Retrieve the field type
         field = properties.get(column_name, {})
         return field.get("type", None)
@@ -256,13 +263,13 @@ class Converter:
         pandas.Series
             Converted attribute column
         """
-        if field_type == 'string':
+        if field_type == "string":
             column = column.apply(lambda x: str(x) if pd.notnull(x) else None)
-        elif field_type == 'integer':
+        elif field_type == "integer":
             column = column.apply(lambda x: int(x) if pd.notnull(x) else None)
-        elif field_type == 'number':
+        elif field_type == "number":
             column = column.apply(lambda x: float(x) if pd.notnull(x) else None)
-        elif field_type == None: # column not part of the schema, so we keep it as is
+        elif field_type == None:  # column not part of the schema, so we keep it as is
             return column
         else:
             raise ValueError(f"Field type {field_type} not supported")
@@ -280,20 +287,13 @@ class Converter:
             Layer to add the NEN3610id column to
         layer_name : str
             Name of the layer
-        
+
         Returns
         -------
         geopandas.GeoDataFrame
             Layer with the NEN3610id column
         """
-        layer_gdf['NEN3610id'] = layer_gdf['code'].apply(lambda x: 'NL.WBHCODE.' + str(WATERSCHAPSCODE) + '.' + layer_name + '.' + str(x))
+        layer_gdf["NEN3610id"] = layer_gdf["code"].apply(
+            lambda x: "NL.WBHCODE." + str(WATERSCHAPSCODE) + "." + layer_name + "." + str(x)
+        )
         return layer_gdf
-        
-    
-
-
-
-
-
-
-
