f2e118f704523277b3feab02463e8cfa6c1b3e77
Esther van der Laan <esthervdl5@gmail.com>
2024-12-24 15:19:00 +0100

    damo_exporter function test en verbeteringen

--- tests/test_damo_exporter.py
+++ tests/test_damo_exporter.py
@@ -1,6 +1,12 @@
 # %%
 """Test for DAMO exporter function"""
 
+import os
+from pathlib import Path
+
+working_dir = Path(__file__).parents[1]
+os.chdir(working_dir)
+
 import geopandas as gpd
 
 from hhnk_threedi_tools.core.schematisation_builder.DAMO_exporter import DAMO_exporter
@@ -23,17 +29,16 @@ def test_DAMO_exporter_one_feature():
 
 
 def test_DAMO_exporter_polders():
-    GPKG_PATH = TEST_DIRECTORY / r"test_damo_exporter/area_test_sql_helsdeur.gpkg"
-    TEST_RESULT_FILE = TEST_DIRECTORY / r"test_damo_exporter/resultaat/DAMO.gpkg"
+    GPKG_PATH = TEST_DIRECTORY / r"test_damo_exporter/input_anna_paulowna/polder_polygon.shp"
+    TEST_RESULT_FILE = TEST_DIRECTORY / r"test_damo_exporter/resultaat/DAMO_anna_paulowna.gpkg"
 
     gebied = gpd.read_file(GPKG_PATH)
-    table_list = ["GEMAAL"]
+    table_list = ["HYDROOBJECT"]
 
     logging = DAMO_exporter(gebied, table_list, TEST_RESULT_FILE)
 
     gdf_result = gpd.read_file(TEST_RESULT_FILE)
 
-    assert gdf_result.loc[0, "code"] == "KGM-Q-29234"
     assert logging == []
 
 
@@ -41,10 +46,7 @@ def test_DAMO_exporter_polders():
 # Test
 if __name__ == "__main__":
     test_DAMO_exporter_one_feature()
+    test_DAMO_exporter_polders()
 
-    # POLDERS_PATH = r"\\corp.hhnk.nl\data\Hydrologen_data\Data\09.modellen_speeltuin\egmondermeer_leggertool\01_source_data\polder_polygon.shp"
-    # POLDERS = gpd.read_file(POLDERS_PATH, engine="pyogrio")
-    # model_extent = POLDERS
-    # output_DAMO = DAMO_exporter(model_extent, ["HYDROOBJECT"])
 
 # %%
