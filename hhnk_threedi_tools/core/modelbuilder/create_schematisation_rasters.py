13d424e2a16079c8c465252503291ddcca801416
Wietse van Gerwen <41104904+wvangerwen@users.noreply.github.com>
2024-12-27 21:36:00 +0100

    improve readability

--- hhnk_threedi_tools/core/modelbuilder/create_schematisation_rasters.py
+++ hhnk_threedi_tools/core/modelbuilder/create_schematisation_rasters.py
@@ -1,10 +1,7 @@
 # %%
-"""Aanmaken rasters voor model"""
-
 from typing import Union
 
 import hhnk_research_tools as hrt
-import numpy as np
 import xarray as xr
 
 from hhnk_threedi_tools.core.folders_modelbuilder import FoldersModelbuilder
@@ -21,14 +18,7 @@ class ClipModelRasterCalc(hrt.RasterCalculatorRxr):
         raster_name: str,
         tempdir: hrt.Folder = None,
     ):
-        r"""Create model raster with nodata outside polder.
-        Bounds based on polder_polygon raster.
-
-        requires these functions from hhnk_threedi_tools\core\modelbuilder\create_calculation_rasters.py
-        to be run first
-        - create_polder_tif
-        - create_waterdeel_tif
-        """
+        r"""Clip raster to polder bounds."""
 
         super().__init__(
             raster_out=raster_out,
@@ -40,79 +30,87 @@ class ClipModelRasterCalc(hrt.RasterCalculatorRxr):
 
     def run(
         self,
-        nodata_keys: list,
-        waterdeel_value=None,
-        dtype="float32",
+        nodata_keys: list[str],
+        dtype: str = "float32",
+        waterdeel_value: Union[float, None] = None,
         chunksize: Union[int, None] = None,
         overwrite: bool = False,
     ):
         """
         Parameters
         ----------
-        waterdeel_value : float, default None
-            Set all values in raster where waterdeel to this value
-        chunksize : Union[int, None], optional
-            Chunksize to perform calculation on.
+        nodata_keys : list of str
+            Keys in raster_paths_dict to to use as nodatamask. This will apply
+            nodata to the outputraster if any of the inputs have nodata.
+        waterdeel_value : float, optional
+            Set raster values to this value where `waterdeel` equals 1 (default: None).
+        dtype : str
+            Data type of the output raster (default: "float32").
+        chunksize : int, optional
+            Chunk size for processing (default: None).
         overwrite : bool, optional
-            Overwrite output
+            Overwrite output (default: False).
         """
-        cont = self.verify(overwrite=overwrite)
+        if not self.verify(overwrite=overwrite):
+            logger.info(f"Skipping creation of {self.raster_name} as overwrite is not allowed.")
+            return
 
-        if cont:
-            # Load DataArrays
-            da_dict = {}
-            for key, r in self.raster_paths_same_bounds.items():
-                da_dict[key] = r.open_rxr(chunksize)
+        # Load data arrays
+        da_dict = {}
+        for key, r in self.raster_paths_same_bounds.items():
+            da_dict[key] = r.open_rxr(chunksize)
 
-            da_out = da_dict[self.raster_name]
+        da_out = da_dict[self.raster_name]
+        crs = da_out.rio.crs  # CRS is lost during xr.where; preserve it.
 
-            crs = da_out.rio.crs  # This is lost when xr.where is used.
+        # Set waterdeel value if specified (default=10 for dem)
+        if waterdeel_value is not None:
+            da_out = xr.where(da_dict["waterdeel"] == 1, waterdeel_value, da_out)
 
-            if waterdeel_value is not None:
-                # waterdeel ophogen naar 10mNAP (default voor dem)
-                da_out = xr.where(da_dict["waterdeel"] == 1, waterdeel_value, da_out)
+        # Create and apply global nodata mask
+        nodata = hrt.variables.DEFAULT_NODATA_VALUES[dtype]
+        da_nodatamasks = self.get_nodatamasks(da_dict=da_dict, nodata_keys=nodata_keys)
+        da_out = xr.where(da_nodatamasks, nodata, da_out)
 
-            # Create global no data mask
-            nodata = hrt.variables.DEFAULT_NODATA_VALUES[dtype]
-
-            da_nodatamasks = self.get_nodatamasks(da_dict=da_dict, nodata_keys=nodata_keys)
-            da_out = xr.where(da_nodatamasks, nodata, da_out)  # Polder extent
-            da_out.rio.set_crs(crs)
-            self.raster_out = hrt.Raster.write(
-                self.raster_out,
-                result=da_out,
-                nodata=nodata,
-                dtype=dtype,
-                chunksize=chunksize,
-            )
-        else:
-            logger.info("Cont is false")
+        # Write to file
+        da_out.rio.set_crs(crs)  # Reapply crs
+        self.raster_out = hrt.Raster.write(
+            self.raster_out,
+            result=da_out,
+            nodata=nodata,
+            dtype=dtype,
+            chunksize=chunksize,
+        )
 
 
-def create_schematisation_rasters(folder_mb: FoldersModelbuilder, pytests=False):
-    """Create schematisation rasters by clipping to model extent"""
+def create_schematisation_rasters(folder_mb: FoldersModelbuilder, pytests: bool = False):
+    r"""Create schematisation rasters by clipping to model extent
+
+    Requires these functions from hhnk_threedi_tools\core\modelbuilder\create_calculation_rasters.py
+    to be run first
+    - create_polder_tif
+    - create_waterdeel_tif
+    """
     tempdir = folder_mb.dst.full_path(f"temp_{hrt.current_time(date=True)}")
 
     # List of raster types to process
     raster_names = ["dem", "glg", "ggg", "ghg", "infiltration", "friction", "landuse"]
 
-    # When running pytest, only try selection
     if pytests:
-        raster_names = ["dem", "landuse"]
+        raster_names = ["dem", "landuse"]  # Reduced list for tests
 
     for raster_name in raster_names:
         # Set parameters based on the raster type
-        waterdeel_value = 10 if raster_name == "dem" else None
-        dtype = "uint8" if raster_name == "landuse" else "float32"
         raster_paths_dict = {
             raster_name: getattr(folder_mb.src, raster_name),
             "polder": folder_mb.dst.polder,
             **({"waterdeel": folder_mb.dst.waterdeel} if raster_name == "dem" else {}),
         }
         nodata_keys = ["polder", raster_name]
+        waterdeel_value = 10 if raster_name == "dem" else None
+        dtype = "uint8" if raster_name == "landuse" else "float32"
 
-        # Initialize the raster calculator
-        self = raster_calc = ClipModelRasterCalc(
+        raster_calc = ClipModelRasterCalc(
             raster_out=getattr(folder_mb.dst, raster_name),
             raster_paths_dict=raster_paths_dict,
             metadata_key="polder",
@@ -120,15 +118,9 @@ def create_schematisation_rasters(folder_mb: FoldersModelbuilder, pytests=False)
             raster_name=raster_name,
         )
 
-        # Run calculation of output raster
         raster_calc.run(
             nodata_keys=nodata_keys,
             waterdeel_value=waterdeel_value,
             dtype=dtype,
             overwrite=False,
         )
-
-
-if __name__ == "__main__":
-    chunksize = None
-    overwrite = False
