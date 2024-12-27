ee7758b79be0ecb3da487917fe9b1911cdee1485
Wietse van Gerwen <41104904+wvangerwen@users.noreply.github.com>
2024-12-27 17:18:35 +0100

    clean schema rasters, working tests

--- hhnk_threedi_tools/core/modelbuilder/create_schematisation_rasters.py
+++ hhnk_threedi_tools/core/modelbuilder/create_schematisation_rasters.py
@@ -3,191 +3,41 @@
 
 from typing import Union
 
-import geopandas as gpd
 import hhnk_research_tools as hrt
 import numpy as np
-import pandas as pd
 import xarray as xr
 
-import hhnk_threedi_tools as htt
+from hhnk_threedi_tools.core.folders_modelbuilder import FoldersModelbuilder
 
 logger = hrt.logging.get_logger(__name__)
-
-
-class ModelbuilderRasters:
-    """This class will create the rasters for a schematisation.
-    The folder attribute contains the links to the source files
-    that will be used to create these rasters.
-    """
-
-    def __init__(
-        self,
-        folder: htt.FoldersModelbuilder,
-        resolution: float = 0.5,
-        nodata: int = -9999,
-        overwrite: bool = False,
-        verbose: bool = False,
-    ):
-        """
-        folder : htt.FoldersModelbuilder
-            folder structure of modelbuilder with src files and dst paths
-        resolution : float, optional, by default 0.5
-            resultion of output raster
-        nodata : int, optional, by default -9999
-            nodata value of output raster
-        overwrite : bool, optional, by default False
-            overwrite the output file if it already exists
-        verbose : bool, optional, by default False
-            print all debug statements
-        """
-        self.folder = folder
-        self.resolution = resolution
-        self.nodata = nodata
-        self.overwrite = overwrite
-        self.verbose = verbose
-
-        # Assigned during function calls.
-        self.dem_calc = None
-
-    def prepare_input(self):
-        """
-        Rasterize polder and waterdeel
-        Create vrt of source dem and gxg with bounds of polder raster.
-        """
-        # Rasterize polder polygon
-        if not self.folder.dst.tmp.polder.exists():
-            gdf_polder = gpd.read_file(self.folder.src.polder.path)
-            metadata = hrt.RasterMetadataV2.from_gdf(gdf=gdf_polder, res=self.resolution)
-            gdf_polder["value"] = 1
-            hrt.gdf_to_raster(
-                gdf=gdf_polder,
-                value_field="value",
-                raster_out=self.folder.dst.tmp.polder,
-                nodata=self.nodata,
-                metadata=metadata,
-                read_array=False,
-            )
-
-        # Rasterize watergangen
-        if not self.folder.dst.tmp.waterdeel.exists():
-            gdf = gpd.read_file(self.folder.src.waterdeel.path)
-            metadata = self.folder.dst.tmp.polder.metadata
-            gdf["value"] = 1
-            hrt.gdf_to_raster(
-                gdf=gdf,
-                value_field="value",
-                raster_out=self.folder.dst.tmp.waterdeel,
-                nodata=self.nodata,
-                metadata=metadata,
-                read_array=False,
-                overwrite=False,
-            )
-
-    def create_rasters(self):
-        """Create output rasters dem and gxg"""
-
-        # Dem creation
-        def run_dem_window(block):
-            """Custom calc function on blocks in hrt.RasterCalculatorV2"""
-            block_out = block.blocks["dem"]
-
-            # waterdeel ophogen naar +10mNAP
-            block_out[block.blocks["waterdeel"] == 1] = 10
-
-            block_out[block.masks_all] = self.nodata
-            return block_out
-
-        # init calculator
-        self.dem_calc = hrt.RasterCalculatorV2(
-            raster_out=self.folder.dst.dem,
-            raster_paths_dict={
-                "dem": self.folder.src.dem,
-                "polder": self.folder.dst.tmp.polder,
-                "waterdeel": self.folder.dst.tmp.waterdeel,
-            },
-            nodata_keys=["polder"],
-            mask_keys=["polder", "dem"],
-            metadata_key="polder",
-            custom_run_window_function=run_dem_window,
-            output_nodata=self.nodata,
-            min_block_size=4096,
-            verbose=self.verbose,
-            tempdir=self.folder.dst.tmp,
-        )
-        # Run calculation of output raster
-        self.dem_calc.run(overwrite=True)
-
-        # raster creation
-        def run_rtype_window(block):
-            """Custom calc function on blocks in hrt.RasterCalculatorV2"""
-            block_out = block.blocks["rtype"]
-
-            # Nodatamasks toepassen
-            block_out[block.masks_all] = self.nodata
-            return block_out
-
-        for rtype in ["glg", "ggg", "ghg", "infiltration", "friction"]:
-            # init calculator
-            raster_calc = hrt.RasterCalculatorV2(
-                raster_out=getattr(self.folder.dst, rtype),
-                raster_paths_dict={
-                    "rtype": getattr(self.folder.src, rtype),
-                    "polder": self.folder.dst.tmp.polder,
-                },
-                nodata_keys=["polder"],
-                mask_keys=["polder", "rtype"],
-                metadata_key="polder",
-                custom_run_window_function=run_rtype_window,
-                output_nodata=self.nodata,
-                min_block_size=4096,
-                verbose=self.verbose,
-                tempdir=self.folder.dst.tmp,
-            )
-            # Run calculation of output raster
-            raster_calc.run(overwrite=False)
-
-    def run(self):
-        self.prepare_input()
-        self.create_rasters()
-
-
-# %%
-# import cProfile
-# import pstats
-
-# profiler = cProfile.Profile()
-# profiler.enable()
-# t()
-# profiler.disable()
-# stats = pstats.Stats(profiler)
-# # stats.strip_dirs()
-# stats.sort_stats("tottime")
-# stats.print_stats()
-
-
-# %% RXR
 
 
 class ClipModelRasterCalc(hrt.RasterCalculatorRxr):
     def __init__(
         self,
         raster_out: hrt.Raster,
-        raster_paths_dict: dict[str : hrt.Raster],
+        raster_paths_dict: dict[str, hrt.Raster],
         metadata_key: str,
-        verbose: bool = False,
+        raster_name: str,
         tempdir: hrt.Folder = None,
     ):
-        """Create model raster with nodata outside polder.
+        r"""Create model raster with nodata outside polder.
         Bounds based on polder_polygon raster.
+
+        requires these functions from hhnk_threedi_tools\core\modelbuilder\create_calculation_rasters.py
+        to be run first
+        - create_polder_tif
+        - create_waterdeel_tif
         """
+
         super().__init__(
             raster_out=raster_out,
             raster_paths_dict=raster_paths_dict,
             nodata_keys=[],  # TODO uitfaseren
             metadata_key=metadata_key,
-            verbose=verbose,
             tempdir=tempdir,
         )
+        self.raster_name = raster_name
 
     def run(self, waterdeel_value=None, chunksize: Union[int, None] = None, overwrite: bool = False):
         """
@@ -196,17 +46,19 @@ class ClipModelRasterCalc(hrt.RasterCalculatorRxr):
         waterdeel_value : float, default None
             Set all values in raster where waterdeel to this value
         chunksize : Union[int, None], optional
-            _description_, by default None
+            Chunksize to perform calculation on.
         overwrite : bool, optional
-            _description_, by default False
+            Overwrite output
         """
         cont = self.verify(overwrite=overwrite)
 
         if cont:
             # Load DataArrays
             da_dict = {}
-            da_out = da_dict["rtype"] = self.raster_paths_same_bounds["rtype"].open_rxr(chunksize)
+            da_out = da_dict[self.raster_name] = self.raster_paths_same_bounds[self.raster_name].open_rxr(chunksize)
             da_polder = da_dict["polder"] = self.raster_paths_same_bounds["polder"].open_rxr(chunksize)  # Nodata key
+
+            crs = da_out.rio.crs  # This is lost when xr.where is used.
 
             if waterdeel_value is not None:
                 # waterdeel ophogen naar 10mNAP (default voor dem)
@@ -218,46 +70,45 @@ class ClipModelRasterCalc(hrt.RasterCalculatorRxr):
 
             da_nodatamasks = self.get_nodatamasks(da_dict=da_dict, nodata_keys=["polder"])
             da_out = xr.where(da_nodatamasks, nodata, da_out)  # Polder extent
-
+            da_out.rio.set_crs(crs)
             self.raster_out = hrt.Raster.write(self.raster_out, result=da_out, nodata=nodata, chunksize=chunksize)
         else:
             logger.info("Cont is false")
 
 
-# %%
-tempdir = folder_mb.dst.full_path(f"temp_{hrt.current_time(date=True)}")
+def create_schematisation_rasters(folder_mb: FoldersModelbuilder, pytests=False):
+    """Create schematisation rasters by clipping to model extent"""
+    tempdir = folder_mb.dst.full_path(f"temp_{hrt.current_time(date=True)}")
 
+    # List of raster types to process
+    raster_names = ["dem", "glg", "ggg", "ghg", "infiltration", "friction"]
 
-for rtype in ["dem", "glg", "ggg", "ghg", "infiltration", "friction"]:
-    # init calculator
-    if rtype == "dem":
-        waterdeel_value = 10
+    for raster_name in raster_names:
+        # Set parameters based on the raster type
+        waterdeel_value = 10 if raster_name == "dem" else None
         raster_paths_dict = {
-            "rtype": getattr(folder_mb.src, rtype),
+            raster_name: getattr(folder_mb.src, raster_name),
             "polder": folder_mb.dst.polder,
-            "waterdeel": folder_mb.dst.waterdeel,
+            **({"waterdeel": folder_mb.dst.waterdeel} if raster_name == "dem" else {}),
         }
 
-    else:
-        waterdeel_value = None
-        raster_paths_dict = {
-            "rtype": getattr(folder_mb.src, rtype),
-            "polder": folder_mb.dst.polder,
-        }
+        # Initialize the raster calculator
+        self = raster_calc = ClipModelRasterCalc(
+            raster_out=getattr(folder_mb.dst, raster_name),
+            raster_paths_dict=raster_paths_dict,
+            metadata_key="polder",
+            tempdir=tempdir,
+            raster_name=raster_name,
+        )
 
-    self = raster_calc = ClipModelRasterCalc(
-        raster_out=getattr(folder_mb.dst, rtype),
-        raster_paths_dict=raster_paths_dict,
-        metadata_key="polder",
-        verbose=verbose,
-        tempdir=tempdir,
-    )
-    # Run calculation of output raster
-    raster_calc.run(waterdeel_value=waterdeel_value, overwrite=False)
+        # Run calculation of output raster
+        raster_calc.run(waterdeel_value=waterdeel_value, overwrite=False)
+
+        # If running pytest, break after the first iteration
+        if pytests:
+            break
 
 
-# %% ENDTEST
 if __name__ == "__main__":
-    verbose = True
     chunksize = None
     overwrite = False
--- tests/test_create_schematisation_rasters.py
+++ tests/test_create_schematisation_rasters.py
@@ -2,19 +2,15 @@
 import importlib
 import shutil
 
+import hhnk_research_tools as hrt
 import pytest
 
 from hhnk_threedi_tools.core import folders_modelbuilder
 from hhnk_threedi_tools.core.folders import Folders
+from hhnk_threedi_tools.core.modelbuilder.create_schematisation_rasters import create_schematisation_rasters
+from tests.config import FOLDER_TEST, TEMP_DIR
 
-# from hhnk_threedi_tools.core.modelbuilder.create_schematisation_rasters import (
-#     ModelbuilderRasters,
-# )
-from tests.config import FOLDER_TEST, PATH_NEW_FOLDER, TEMP_DIR
-
-importlib.reload(folders_modelbuilder)
-
-
+PATH_NEW_FOLDER = TEMP_DIR / f"test_project_{hrt.current_time(date=True)}"
 FOLDER_NEW = Folders(PATH_NEW_FOLDER, create=True)
 shutil.copytree(FOLDER_TEST.source_data.path, FOLDER_NEW.source_data.path, dirs_exist_ok=True)
 shutil.copy(FOLDER_TEST.model.calculation_rasters.polder.path, FOLDER_NEW.model.calculation_rasters.polder.path)
@@ -41,19 +37,10 @@ def test_create_schematisation_rasters():
 
     assert folder_mb.dst.dem.exists() is False
 
-    resolution = 0.5
-    nodata = -9999
-    overwrite = False
-    verbose = True
-    chunksize = None
-    self = model_rasters = ModelbuilderRasters(
-        folder=folder, resolution=resolution, nodata=nodata, overwrite=overwrite, verbose=verbose
-    )
+    create_schematisation_rasters(folder_mb=folder_mb, pytests=True)
 
-    model_rasters.run()
-
-    assert folder.dst.dem.exists() is True
-    assert folder.dst.dem.shape == [6962, 7686]
+    assert folder_mb.dst.dem.exists() is True
+    assert folder_mb.dst.dem.shape == [6962, 7686]
 
 
 # %%
