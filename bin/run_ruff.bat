90e3af36cb04474b86a5387a833850283b3f90ee
Wietse van Gerwen <41104904+wvangerwen@users.noreply.github.com>
2025-01-24 18:45:26 +0100

    Klimaatsommen tests opzet (#134)
    * test tot maskerkaart
    
    * cont false
    
    * ruff
    
    * ruff again
    
    * revert wrong merge changes
    
    * more reverts
    
    * get_array to rxr

--- bin/run_ruff.bat
+++ bin/run_ruff.bat
@@ -1,2 +1,3 @@
 REM force-exclude to make sure it uses the extend-exclude from pyproject.
-python -m ruff format ../hhnk_threedi_tools/**/*.py --force-exclude
+python -m ruff check ../hhnk_threedi_tools --select I --fix 
+python -m ruff format ../hhnk_threedi_tools --force-exclude
