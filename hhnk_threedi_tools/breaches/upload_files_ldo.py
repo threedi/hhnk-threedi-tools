49394f0fd2127c07699de80b3b7aa1585d565b06
Gerwen <41104904+wvangerwen@users.noreply.github.com>
2025-02-07 17:03:24 +0100

    refactor access token

--- hhnk_threedi_tools/breaches/upload_files_ldo.py
+++ hhnk_threedi_tools/breaches/upload_files_ldo.py
@@ -61,7 +61,7 @@ import pandas as pd
 import requests
 from breaches import Breaches
 
-LDO_API_URL = "https://www.overstromingsinformatie.nl/auth/"
+LDO_API_URL = "https://www.overstromingsinformatie.nl/api/v1/"
 
 # Generate api key on de LDO_API_URL website. And place it in api_ldo_key.txt
 LDO_API_KEY = Path("api_ldo_key.txt").read_text()
@@ -88,55 +88,46 @@ parameters = {
 
 
 # %%
-class LDO_API_AUTH:
-    def __init__(self, url_auth, api_key, tenant=4):
-        self.url_auth = url_auth
+class LDO_API:
+    def __init__(self, api_key=LDO_API_KEY, tenant=4, url=LDO_API_URL):
+        self.url = url
         self.api_key = api_key
         self.tenant = tenant  # organisation, 4=hhnk.
 
-        self.token = None  # set on calling refresh_token
-        self._refresh_token = None  # Property
+        # Authorisation goes through the auth endpoint
+        self.url_auth = self.url.replace("/api/", "/auth/")
+
+        self._refresh_token = None  # set on calling self.access_token
+        self._access_token = None  # Property
 
     @property
-    def refresh_token(self):
+    def access_token(self):
         """Get refresh token so we can interact with the api."""
         if self._refresh_token is None:
-            # Token is required to get the refresh_token
-            token_url = self.url_auth + "v1/token/"
-            self.token = (
-                requests.post(
-                    url=token_url,
-                    json={"tenant": self.tenant},
-                    auth=("__key__", self.api_key),
-                    timeout=5,
-                )
-            ).json()["refresh"]
-
-            url_refresh = self.url_auth + "v1/token/refresh/"
-            self._refresh_token = (
-                requests.post(
-                    url=url_refresh,
-                    json={"refresh": self.token},
-                    auth=("__key__", self.api_key),
-                    timeout=5,
-                )
-            ).json()["access"]
-        return self._refresh_token
+            r = requests.post(
+                url=self.url_auth + "token/",
+                json={"tenant": self.tenant},
+                auth=("__key__", self.api_key),
+                timeout=5,
+            ).json()
+            self._refresh_token = r["refresh"]
+            self._access_token = r["access"]
+        return self._access_token
 
     def get_tenants(self):
         """Tenant / organisation which has an id and name.
 
         Prints the tenants. The id can be used to get the token.
         """
-        tenant_url = self.url_auth + "v1/tenants/"
+        tenant_url = self.url + "v1/tenants/"
         tenants = requests.get(url=tenant_url, auth=("__key__", self.api_key), timeout=5).json()
         for tenant in tenants:
             logger.info(tenant)
         return tenants
 
     def test_api(self):
-        """Test api connection"""
-        health_url = self.url_auth[:-5] + "/health/"  # Health is not under v1.
+        """Test api connection without requiring authorisation"""
+        health_url = self.url[:-4] + "/health/"  # Health is not under v1.
         response_health = requests.get(url=health_url, timeout=5)
         assert response_health.status_code == 200
 
@@ -144,29 +135,35 @@ class LDO_API_AUTH:
 # %%
 
 
-class SelectFolder(hrt.Folder):
-    """
-    An object to ease the accessibility, creation and checks of folders and
-    files that need to be uploaded to LDO.
+class UploadFolder(hrt.Folder):
+    def __init__(self, base, scenario_results_path, create=True):
+        """
+        Folder to store files that need to be uploaded to LDO.
 
-    Main Folder object
-        ├── dem.tif
-        ├── results_3di.nc
-    """
+        Folder
+            ├── dem.tif
+            ├── results_3di.nc
+            ├── Folder.zip (zipped dem.tif + results_3di.nc)
 
-    def __init__(self, base, scenario_output_path, create=True):
+        Parameters
+        ----------
+        base : Union[str,Path]
+            output directory where upload files will be placed
+        scenario_results_path
+            directory where scenario results are stored.
+        """
         super().__init__(base, create=create)
-        self.scenario_output_path = scenario_output_path
-        self.zip_kb = None
+        self.scenario_results_path = scenario_results_path
+        self.zip_size = None
         self.zip_path = None
 
     def _find_scenario_folder(self):
         """Get Path to scenario results"""
-        scenario_paths = [j for i in self.scenario_output_path.glob("*/") for j in list(i.glob("*/"))]
+        scenario_paths = [j for i in self.scenario_results_path.glob("*/") for j in list(i.glob("*/"))]
         for scenario_path in scenario_paths:
             if scenario_path.name == self.name:
                 return scenario_path
-        raise FileNotFoundError(f"{self.name} not found in {self.scenario_output_path}")
+        raise FileNotFoundError(f"{self.name} not found in {self.scenario_results_path}")
 
     def copy_files(self):
         """Copy NetCDF and DEM to the upload folder"""
@@ -193,8 +190,8 @@ class SelectFolder(hrt.Folder):
                 if file.name != zip_name:
                     zipf.write(file, arcname=file.name)
 
-        self.zip_kb = round(self.zip_path.stat().st_size / 1024 / 1024, 2)  # MB
-        logger.info(f"Zip {zip_name} created with size {self.zip_kb} MB")
+        self.zip_size = round(self.zip_path.stat().st_size / 1024 / 1024, 2)  # MB
+        logger.info(f"Zip {zip_name} created with size {self.zip_size} MB")
         return self.zip_path
 
 
@@ -211,7 +208,7 @@ class LDO_API_UPLOAD:
             "authorization": f"Bearer {self.refresh_token}",
             # 'content-type':'multiplart/form-data',
         }
-        self.url_uploadfile = "https://www.overstromingsinformatie.nl/api/v1/excel-imports"
+        self.url_uploadfile = "/excel-imports"
         self.scenario_id = None
         self.id_excel = None
 
@@ -273,7 +270,7 @@ if __name__ == "__main__":
     id_scenarios = r"E:\03.resultaten\Overstromingsberekeningenprimairedoorbraken2024\ldo_structuur\scenarios_ids.xlsx"
     # Folder location where the scenarios are going to be copied
     ldo_structuur_path = r"E:\03.resultaten\Overstromingsberekeningenprimairedoorbraken2024\ldo_structuur"
-    scenario_output_path = Path(r"E:\03.resultaten\Overstromingsberekeningenprimairedoorbraken2024\output")
+    scenario_results_path = Path(r"E:\03.resultaten\Overstromingsberekeningenprimairedoorbraken2024\output")
 
     # List the scenario name to be uploaded
     scenario_names = os.listdir(metadata_folder)
@@ -303,17 +300,15 @@ if __name__ == "__main__":
             path = hrt.Folder(os.path.join(ldo_structuur_path, scenario_name))
 
             # Create folder with data to upload.
-            ldo_structuur = SelectFolder(path)
+            ldo_structuur = UploadFolder(path, scenario_results_path=scenario_results_path)
             ldo_structuur.copy_files()
             ldo_structuur.zip_files()
 
-            # Zip the file and retrieve the path of its location
-
             # Set API key
-            ldo_api = LDO_API_AUTH(url_auth=LDO_API_URL, api_key=LDO_API_KEY)
+            ldo_api = LDO_API(api_key=LDO_API_KEY)
 
             # Set UPLOAD as an object
-            ldo_upload = LDO_API_UPLOAD(metadata_folder, ldo_api.refresh_token, scenario_name)
+            ldo_upload = LDO_API_UPLOAD(metadata_folder, ldo_api.access_token, scenario_name)
 
             # get metadata file of the scenario that is been uploaded
             metadata_file = ldo_upload.metadata_file
@@ -333,7 +328,9 @@ if __name__ == "__main__":
             pd_scenarios.loc[pd_scenarios["Naam van het scenario"] == scenario_name, "ID_SCENARIO"] = scenario_id
 
             # Save  the size of the scenario in the metdata dataframe
-            pd_scenarios.loc[pd_scenarios["Naam van het scenario"] == scenario_name, "SIZE_KB"] = ldo_structuur.zip_kb
+            pd_scenarios.loc[pd_scenarios["Naam van het scenario"] == scenario_name, "SIZE_KB"] = (
+                ldo_structuur.zip_size
+            )
 
             # REMOVE/DELETE ZIP AND FOLDER FROM THE SCENARIO THAT IS ALREADY UPLOADED.
             delete_file.append(ldo_structuur.path)
