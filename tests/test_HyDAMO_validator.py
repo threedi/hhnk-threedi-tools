7d33428356e79e705636304797a08595a01349f7
wvangerwen <41104904+wvangerwen@users.noreply.github.com>
2025-04-21 12:55:21 +0200

    add hydamo_validation to git action
    removed folder changing. Should be installed as editable.

--- tests/test_HyDAMO_validator.py
+++ tests/test_HyDAMO_validator.py
@@ -1,27 +1,19 @@
-import os
+# %%
 import shutil
 from pathlib import Path
-
-import geopandas as gpd
-import numpy as np
 
 from tests.config import TEST_DIRECTORY
 
 
 def test_HyDAMO_validator():
-    # TODO temp method of import validatiemodule, change workdir
-    folder = Path(__file__).parents[2] / "HyDAMOValidatieModule"
-    os.chdir(folder)
     from hhnk_threedi_tools.core.schematisation_builder.HyDAMO_validator import validate_hydamo
 
-    os.chdir(Path(__file__).parent)  # change workdir back to this file
+    validation_directory_path = TEST_DIRECTORY / "test_HyDAMO_validator"
+    hydamo_file_path = validation_directory_path / "HyDAMO.gpkg"
+    validation_rules_json_path = validation_directory_path / "rules.json"
 
-    TEST_DIRECTORY_HyDAMO_validator = TEST_DIRECTORY / "test_HyDAMO_validator"
-    HyDAMO_path = TEST_DIRECTORY_HyDAMO_validator / "HyDAMO.gpkg"
-    validation_rules_json_path = TEST_DIRECTORY_HyDAMO_validator / "rules.json"
-
-    if not HyDAMO_path.exists():
-        raise FileNotFoundError(f"File {HyDAMO_path} does not exist")
+    if not hydamo_file_path.exists():
+        raise FileNotFoundError(f"File {hydamo_file_path} does not exist")
     if not validation_rules_json_path.exists():
         raise FileNotFoundError(f"File {validation_rules_json_path} does not exist")
 
@@ -30,9 +22,9 @@ def test_HyDAMO_validator():
         shutil.copytree(r"D:/github/overmeen/data/test_HyDAMO_validator/dtm", test_coverage_location)
 
     validate_hydamo(
-        hydamo_file_path=HyDAMO_path,
+        hydamo_file_path=hydamo_file_path,
         validation_rules_json_path=validation_rules_json_path,
-        validation_directory_path=TEST_DIRECTORY_HyDAMO_validator,
+        validation_directory_path=validation_directory_path,
         coverages_dict={"AHN": test_coverage_location},
         output_types=["geopackage", "csv", "geojson"],
     )
@@ -40,7 +32,6 @@ def test_HyDAMO_validator():
     # TODO some checks on the output
 
 
-# %%
 if __name__ == "__main__":
     test_HyDAMO_validator()
 
