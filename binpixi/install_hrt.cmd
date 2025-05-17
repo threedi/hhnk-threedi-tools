74350547d53156c9e693177bffd9592b55a0aa92
wvangerwen <41104904+wvangerwen@users.noreply.github.com>
2025-05-17 15:23:36 +0200

    cleanup unused

--- binpixi/install_hrt.cmd
+++ binpixi/install_hrt.cmd
@@ -1,8 +1,5 @@
 @ECHO OFF
 
-@REM IF NOT EXISTS "..\hhnk-theedi-tools"
-@REM htt = "pip install --no-build-isolation --no-deps --disable-pip-version-check -e ../hhnk-threedi-tools"
-
 IF NOT EXIST "..\hhnk-research-tools" (
     git clone https://github.com/HHNK/hhnk-research-tools/ ..\hhnk-research-tools
 )
