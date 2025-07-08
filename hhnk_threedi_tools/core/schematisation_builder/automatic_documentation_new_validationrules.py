97c7a5b4223bab1c631f4097dce9d9b0212209e5
Esther van der Laan <esthervdl5@gmail.com>
2025-07-08 09:40:46 +0200

    update validation rules descriptions and convert new rules to csv format

--- hhnk_threedi_tools/core/schematisation_builder/automatic_documentation_new_validationrules.py
+++ hhnk_threedi_tools/core/schematisation_builder/automatic_documentation_new_validationrules.py
@@ -13,24 +13,32 @@ with open(validation_rules_json_path) as val:
 
 
 # create empty dataframe
-table_rules = pd.DataFrame(columns=["laag", "type functie", "naam", "beschrijving"])
+table_rules = pd.DataFrame(columns=["laag", "type_functie", "naam", "beschrijving"])
 
 for object_layer in val_rules["objects"]:
-    for general_function in object_layer["general_rules"]:
-        if general_function["id"] >= 100:
-            new_func = {
-                "laag": object_layer["object"],
-                "type_function": "general_rules",
-                "naam": general_function["name"],
-                "beschrijving": general_function["description"],
-            }
-            # TODO: voeg new rule toe aan dataframe, weet code effe niet
+    if object_layer.get("general_rules"):
+        for general_function in object_layer["general_rules"]:
+            # print(general_function)
+            if general_function["id"] >= 100:
+                new_func = {
+                    "laag": object_layer["object"],
+                    "type_functie": "general rules",
+                    "naam": general_function["result_variable"],
+                    "beschrijving": general_function["description"],
+                }
+                # TODO: voeg new rule toe aan dataframe, weet code effe niet
+                table_rules = pd.concat([table_rules, pd.DataFrame([new_func])], ignore_index=True)
+
     for val_rule in object_layer["validation_rules"]:
         if val_rule["validation_rule_set"] == "hhnk":
             new_rule = {
                 "laag": object_layer["object"],
-                "type_function": "validation_rules",
+                "type_functie": "validation rules",
                 "naam": val_rule["name"],
                 "beschrijving": val_rule["description"],
             }
             # TODO: voeg toe
+            table_rules = pd.concat([table_rules, pd.DataFrame([new_rule])], ignore_index=True)
+
+# save to csv
+table_rules.to_csv(hrt.get_pkg_resource_path(schematisation_builder_resources, "hhnk_validationrules.csv"))
