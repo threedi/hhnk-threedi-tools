c1f6624a9c490ef989eb2bb1796e529e10980050
jcacosta89 <78262040+jcacosta89@users.noreply.github.com>
2025-07-09 17:16:47 +0200

    transform polygon to line to later on make spatial join witht the gemaal that are close to 10 cm.
    Create a column distance_validation_rule that is going to be use in  the validation_rule.json module.

---
+++ hhnk_threedi_tools/core/schematisation_builder/gemaal_intermediate_gebied_join.py
@@ -0,0 +1,70 @@
+# %%
+from pathlib import Path
+
+import fiona
+import geopandas as gpd
+import matplotlib.pyplot as plt
+
+damo_path = Path(r"E:\09.modellen_speeltuin\test_with_pomp_table_juan\01_source_data\DAMO.gpkg")
+polder_polygon = Path(
+    r"\\corp.hhnk.nl\data\Hydrologen_data\Data\09.modellen_speeltuin\test_with_pomp_table_juan\01_source_data\polder_polygon.shp"
+)
+output_path = Path(
+    r"E:\09.modellen_speeltuin\test_with_pomp_table_juan\01_source_data\peilgebiedpraktijk_linestring.gpkg"
+)
+
+# read layers as a geopandas dataframe
+damo_gdf = gpd.read_file(damo_path)
+polder_polygon_gdf = gpd.read_file(polder_polygon)
+layer_names = fiona.listlayers(damo_path)
+
+
+# get the layer gemaal een pielgebiedpraktijk
+def get_layer_gdf(layer_name):
+    return gpd.read_file(damo_path, layer=layer_name)
+
+
+gdf_gemaal = get_layer_gdf("GEMAAL")
+gdf_peilgebiedpraktijk = get_layer_gdf("peilgebiedpraktijk")
+
+# transform MULTIPOLYGON  to LINESTRING
+gdf_peilgebiedpraktijk_linestring = gdf_peilgebiedpraktijk.copy()
+gdf_peilgebiedpraktijk_linestring["geometry"] = gdf_peilgebiedpraktijk_linestring["geometry"].apply(
+    lambda geom: geom.boundary if geom.type == "MultiPolygon" else geom
+)
+gdf_peilgebiedpraktijk_linestring = gdf_peilgebiedpraktijk_linestring.explode(index_parts=True)
+gdf_peilgebiedpraktijk_linestring = gdf_peilgebiedpraktijk_linestring.reset_index(drop=True)
+
+# clip linestrings to the polder_polygon
+gdf_peilgebiedpraktijk_linestring = gpd.clip(gdf_peilgebiedpraktijk_linestring, polder_polygon_gdf)
+
+# save the clipped linestring to a new file
+gdf_peilgebiedpraktijk_linestring.to_file(output_path, driver="GPKG")
+
+# make spatial join between gdf_gemaal and gdf_peilgebiedpraktijk_linestring
+gemaal_spatial_join = gpd.sjoin_nearest(
+    gdf_gemaal,
+    gdf_peilgebiedpraktijk_linestring,
+    how="inner",
+    max_distance=0.01,
+    distance_col="distance_validation_rule",
+)
+
+# Join the column 'distance_validation_rule' from gemaal_spatial_join into gdf_gemaal based on the 'code' column
+gdf_gemaal = gdf_gemaal.merge(gemaal_spatial_join[["code", "distance_validation_rule"]], on="code", how="left")
+
+# Loop over gdf_gemaal and print rows where 'distance_validation_rule' is lower than 0.01
+for idx, row in gdf_gemaal.iterrows():
+    if row["distance_validation_rule"] < 0.01:
+        print(row)
+
+
+# %%
+# plot the geometry of gdf_gemaal, gdf_peilgebiedpraktijk_linestring and a
+ax = gdf_peilgebiedpraktijk_linestring.plot(
+    color="blue", edgecolor="black", figsize=(10, 10), label="Peilgebiedpraktijk Linestring"
+)
+gdf_gemaal.plot(ax=ax, color="red", markersize=5, label="Gemaal")
+plt.legend()
+plt.show()
+# %%
