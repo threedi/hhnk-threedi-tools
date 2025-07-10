5826fd8299948a4885e5e4ffd29f25314e1a0d77
jcacosta89 <78262040+jcacosta89@users.noreply.github.com>
2025-07-10 11:53:56 +0200

    Add polygon to polyline conversion and spatial join
    Introduced a method to convert polder polygons to polylines, clip them, and perform a spatial join with the gemaal layer to calculate 'distance_validation_rule'. Updated the PompIntermediateConverter to load additional layers and handle the new polder polygon input.

--- hhnk_threedi_tools/core/schematisation_builder/gemaal_intermediate_converter.py
+++ hhnk_threedi_tools/core/schematisation_builder/gemaal_intermediate_converter.py
@@ -11,25 +11,34 @@ import pandas as pd
 import pyogrio
 
 import hhnk_threedi_tools as htt
+from hhnk_threedi_tools.core.folders import Folders
 
 # %%
+
+
+def __init__(self, folder: Folders, revision=0, dem_path=None):
+    self.folder = folder
+    self.revision = revision
 
 
 class PompIntermediateConverter:
     """
     Intermediate converter for pomp data.
     From CSO format to DAMO/intermediate format, ready for converting to HyDAMO.
-
+    Add column "distance_validation_rule" to the gemaal layer.
     Parameters
     ----------
     damo_file_path : Path
         Path to the DAMO geopackage file.
+    poler_polygon :  Path
+        Path to the poler_polygon
     logger : logging.Logger, optional
         Logger for logging messages. If not provided, a default logger will be used.
     """
 
-    def __init__(self, damo_file_path: Path, logger=None):
+    def __init__(self, damo_file_path: Path, polder_polygon_path: Path, logger=None):
         self.damo_file_path = Path(damo_file_path)
+        self.polder_polygon_path = Path(polder_polygon_path)
 
         if logger:
             self.logger = logger
@@ -38,6 +47,8 @@ class PompIntermediateConverter:
 
         self.pomp: Optional[gpd.GeoDataFrame] = None
         self.gemaal: Optional[gpd.GeoDataFrame] = None
+        self.combinatiepeilgebied: Optional[gpd.GeoDataFrame] = None
+        self.polder_polygon: Optional[gpd.GeoDataFrame] = None
 
         self.logger.info(f"Initialized GemaalIntermediateConverter with DAMO file: {self.damo_file_path}")
 
@@ -46,8 +57,15 @@ class PompIntermediateConverter:
         self.logger.info("Loading pomp layer from DAMO file...")
         self.pomp = gpd.read_file(self.damo_file_path, layer="POMP")
         # self.logger(self.pomp.head())
+
         self.gemaal = gpd.read_file(self.damo_file_path, layer="GEMAAL")
         self.logger.info("Pomp layer loaded successfully.")
+
+        self.combinatiepeilgebied = gpd.read_file(self.damo_file_path, layer="COMBINATIEPEILGEBIED")
+        self.logger.info("Combinatiepeilgebied layer loaded successfully.")
+
+        self.polder_polygon = gpd.read_file(self.polder_polygon_path)
+        self.logger.info("Poler polygon layer loaded successfully.")
 
     def add_column_gemaalid(self):
         """Add gemaalid column to the pomp layer."""
@@ -101,6 +119,36 @@ class PompIntermediateConverter:
         # Save the updated pomp layer to the DAMO geopackage
         pyogrio.write_dataframe(self.pomp, self.damo_file_path, layer="pomp", driver="GPKG", overwrite=True)
         self.logger.info("Pomp layer updated successfully.")
+
+    def polygon_to_polyline(self):
+        # transform MULTIPOLYGON  to LINESTRING
+        self.polder_polygon
+        gdf_peilgebiedpraktijk_linestring = self.combinatiepeilgebied.copy()
+        gdf_peilgebiedpraktijk_linestring["geometry"] = gdf_peilgebiedpraktijk_linestring["geometry"].apply(
+            lambda geom: geom.boundary if geom.type == "MultiPolygon" else geom
+        )
+        gdf_peilgebiedpraktijk_linestring = gdf_peilgebiedpraktijk_linestring.explode(index_parts=True)
+        gdf_peilgebiedpraktijk_linestring = gdf_peilgebiedpraktijk_linestring.reset_index(drop=True)
+
+        # clip linestrings to the polder_polygon
+        gdf_peilgebiedpraktijk_linestring = gpd.clip(gdf_peilgebiedpraktijk_linestring, self.polder_polygon)
+
+        # make spatial join between gdf_gemaal and gdf_peilgebiedpraktijk_linestring distance 10 cm
+        gemaal_spatial_join = gpd.sjoin_nearest(
+            self.gemaal,
+            gdf_peilgebiedpraktijk_linestring,
+            how="inner",
+            max_distance=0.01,
+            distance_col="distance_validation_rule",
+        )
+
+        # Join the column 'distance_validation_rule' from gemaal_spatial_join into gdf_gemaal based on the 'code' column
+        self.gdf_gemaal = self.gemaal.merge(
+            gemaal_spatial_join[["code", "distance_validation_rule"]], on="code", how="left"
+        )
+
+        # save the layer in DAMO TOFIX """"SHOULD I SAVE IT IN DAMO OR HYDAMO"""
+        self.gdf_gemaal.to_file(self.damo_file_path, layer="GEMAAL", driver="GPKG")
 
 
 # %%
