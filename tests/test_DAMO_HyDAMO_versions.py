ac871535e6e4d60c722e5a7570d6c752fa82296c
jzitman <jelle.zitman@nelen-schuurmans.nl>
2025-07-31 12:13:25 +0200

    Conversion of column profieltype/typeprofiel to soortmeetpunt

--- tests/test_DAMO_HyDAMO_versions.py
+++ tests/test_DAMO_HyDAMO_versions.py
@@ -20,24 +20,35 @@ from tests.config import TEMP_DIR, TEST_DIRECTORY
 LAYERS = ["profielpunt"]
 temp_dir_out = TEMP_DIR / f"temp_DAMO_HyDAMO_converter_{hrt.current_time(date=True)}"
 
-
-def test_DAMO_HyDAMO_versions():
+def test_DAMO_HyDAMO_versions(damo_version="2.3", hydamo_version="2.3"):
     damo_file_path = TEST_DIRECTORY / "schema_builder" / "DAMO.gpkg"
     hydamo_file_path = temp_dir_out / "HyDAMO.gpkg"
 
     assert damo_file_path.exists()
     damo_layers = fiona.listlayers(damo_file_path)
     assert all([layer in damo_layers for layer in LAYERS])
+
     DAMO_profielpunt_gdf = gpd.read_file(damo_file_path, layer=LAYERS[0])
 
-    damo_version = hydamo_version = None
     if "typeProfielPunt" in DAMO_profielpunt_gdf.columns:
         damo_version = "2.3"
         hydamo_version = "2.3"
-    if "profielTypePunt" in DAMO_profielpunt_gdf.columns:
+        DAMO_profielpunt_gdf.rename(columns={"typeProfielPunt": "soortMeetpunt"}, inplace=True)
+        DAMO_profielpunt_gdf.to_file(damo_file_path, layer=LAYERS[0])
+        print("Renamed column typeProfielPunt to soortMeetpunt")
+    elif "profielTypePunt" in DAMO_profielpunt_gdf.columns:
         damo_version = "2.4.1"
         hydamo_version = "2.4"
+        DAMO_profielpunt_gdf.rename(columns={"profielTypePunt": "soortMeetpunt"}, inplace=True)
+        DAMO_profielpunt_gdf.to_file(damo_file_path, layer=LAYERS[0])
+        print("Renamed column profielTypePunt to soortMeetpunt")
+    elif "soortMeetpunt" in DAMO_profielpunt_gdf.columns:
+        print("soortMeetpunt present in columns")
+    else:
+        damo_version = hydamo_version = None
+
     assert damo_version != None and hydamo_version != None
+    print(f"DAMO version: {damo_version}, HyDAMO version: {hydamo_version}")
 
     converter = DAMO_to_HyDAMO_Converter(
         damo_file_path=damo_file_path,
@@ -45,7 +56,7 @@ def test_DAMO_HyDAMO_versions():
         damo_version=damo_version,
         hydamo_version=hydamo_version,
         layers=LAYERS,
-        overwrite=False,
+        overwrite=True,
     )
     converter.run()
 
@@ -54,28 +65,28 @@ def test_DAMO_HyDAMO_versions():
     row_id = None
 
     if damo_version == "2.3":
-        assert "typeProfielPunt" in DAMO_profielpunt_gdf.columns
-        assert DAMO_profielpunt_gdf["typeProfielPunt"].apply(lambda x: isinstance(x, (int))).all()
+        assert "soortMeetpunt" in DAMO_profielpunt_gdf.columns
+        assert DAMO_profielpunt_gdf["soortMeetpunt"].apply(lambda x: isinstance(x, (int))).all()
         for i, row in DAMO_profielpunt_gdf.iterrows():
-            if row["typeProfielPunt"] == 1:
+            if row["soortMeetpunt"] == 1:
                 row_id = i
     if hydamo_version == "2.3":
-        assert "typeprofielpunt" in HyDAMO_profielpunt_gdf.columns
-        assert HyDAMO_profielpunt_gdf["typeprofielpunt"].apply(lambda x: isinstance(x, (str))).all()
+        assert "soortmeetpunt" in HyDAMO_profielpunt_gdf.columns
+        assert HyDAMO_profielpunt_gdf["soortmeetpunt"].apply(lambda x: isinstance(x, (str))).all()
         if row_id:
-            assert HyDAMO_profielpunt_gdf.loc[row_id, "typeprofielpunt"] == "linker insteek landzijde"
+            assert HyDAMO_profielpunt_gdf.loc[row_id, "soortmeetpunt"] == "linker insteek landzijde"
 
     if damo_version == "2.4.1":
-        assert "profielTypePunt" in DAMO_profielpunt_gdf.columns
-        assert DAMO_profielpunt_gdf["profielTypePunt"].apply(lambda x: isinstance(x, (int))).all()
+        assert "soortMeetpunt" in DAMO_profielpunt_gdf.columns
+        assert DAMO_profielpunt_gdf["soortMeetpunt"].apply(lambda x: isinstance(x, (int))).all()
         for i, row in DAMO_profielpunt_gdf.iterrows():
-            if row["profielTypePunt"] == 1:
+            if row["soortMeetpunt"] == 1:
                 row_id = i
     if hydamo_version == "2.4":
-        assert "profieltypepunt" in HyDAMO_profielpunt_gdf.columns
-        assert HyDAMO_profielpunt_gdf["profieltypepunt"].apply(lambda x: isinstance(x, (str))).all()
+        assert "soortmeetpunt" in HyDAMO_profielpunt_gdf.columns
+        assert HyDAMO_profielpunt_gdf["soortmeetpunt"].apply(lambda x: isinstance(x, (str))).all()
         if row_id:
-            assert HyDAMO_profielpunt_gdf.loc[row_id, "profieltypepunt"] == "linker insteek sloot binnendijks"
+            assert HyDAMO_profielpunt_gdf.loc[row_id, "soortmeetpunt"] == "linker insteek sloot binnendijks"
 
 
 # %%
