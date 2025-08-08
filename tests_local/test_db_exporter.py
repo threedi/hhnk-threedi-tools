f9d3fad1bc92447e7b103c067561a57e62e9def3
Wouter van Esse <48908991+wvanesse@users.noreply.github.com>
2025-08-08 15:12:56 +0200

    test domains

--- tests_local/test_db_exporter.py
+++ tests_local/test_db_exporter.py
@@ -83,6 +83,31 @@ def test_db_exporter_polder():
     assert gdf_result.shape[0] > 0
 
 
+def test_db_exporter_domains():
+    """Test the db_exporter function using all defeault tables for the test polder."""
+
+    model_extent_path = TEST_DIRECTORY / r"model_test\01_source_data\polder_polygon.shp"
+    output_file = db_export_output_dir / "test_export_domain.gpkg"
+
+    model_extent_gdf = gpd.read_file(model_extent_path, engine="pyogrio")
+    table_names = ["HYDROOBJECT"]
+
+    logging_DAMO = db_exporter(
+        model_extent_gdf=model_extent_gdf,
+        table_names=table_names,
+        output_file=output_file,
+    )
+
+    assert output_file.exists() is True
+
+    gdf_result = gpd.read_file(output_file, engine="pyogrio")
+
+    # assert soort vak is omgezet
+    assert "kunstwerkvak" in gdf_result["ws_soort_vak"].unique()
+
+    assert logging_DAMO == []
+
+
 # %%
 # Test
 if __name__ == "__main__":
@@ -90,6 +115,7 @@ if __name__ == "__main__":
     test_db_exporter_one_feature()
     test_db_exporter_GEMAAL_and_POMP_from_CSO()
     test_db_exporter_polder()
+    test_db_exporter_domains()
 
 
 # %%
