abcd52dfc32b16edbe3368261f53a4e8b42c14cf
wvanesse <48908991+wvanesse@users.noreply.github.com>
2025-09-25 14:52:53 +0200

    new class in ini

--- hhnk_threedi_tools/__init__.py
+++ hhnk_threedi_tools/__init__.py
@@ -15,7 +15,7 @@ from hhnk_threedi_tools.core.checks.bank_levels import BankLevelTest
 
 # FIXME TypeError: metaclass conflict: the metaclass of a derived class must be a (non-strict) subclass of the metaclasses of all its bases
 from hhnk_threedi_tools.core.checks.one_d_two_d import OneDTwoDTest
-from hhnk_threedi_tools.core.checks.sqlite.sqlite_main import SqliteCheck  # FIXME
+from hhnk_threedi_tools.core.checks.sqlite.sqlite_main import HhnkSchematisationCheck  # FIXME
 from hhnk_threedi_tools.core.checks.zero_d_one_d import ZeroDOneDTest
 from hhnk_threedi_tools.core.folders import Folders
 from hhnk_threedi_tools.core.folders_modelbuilder import FoldersModelbuilder
