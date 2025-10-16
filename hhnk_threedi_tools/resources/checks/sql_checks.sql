773adaabc6fa01dcfebcf631a0c0067d336bbf52
Wouter van Esse <48908991+wvanesse@users.noreply.github.com>
2025-10-16 10:45:02 +0200

    sql chacks werkend nieuwe schema

--- hhnk_threedi_tools/resources/checks/sql_checks.sql
+++ hhnk_threedi_tools/resources/checks/sql_checks.sql
@@ -1,271 +1,242 @@
-SELECT 'v2_cross_section_definition' as table_name,
-v2_cross_section_definition.id as id,
+SELECT 'cross_section_location' as table_name,
+cross_section_location.id as id,
 'WARNING: cross section definition height not used for shape type 1' as error
-FROM v2_cross_section_definition
-WHERE 'height' IS NOT NULL AND 'shape' = 1
+FROM cross_section_location
+WHERE 'cross_section_height' IS NOT NULL AND 'cross_section_shape' = 1
 UNION ALL
-SELECT 'v2_cross_section_definition' as table_name,
-v2_cross_section_definition.id as id,
+SELECT 'cross_section_location' as table_name,
+cross_section_location.id as id,
 'ERROR: multiple height and width entries must have the same count' as error
-FROM v2_cross_section_definition
-WHERE length('height') - length(replace('height', ' ', '')) <> length('width') - length(replace('width', ' ', ''))
+FROM cross_section_location
+WHERE length('cross_section_height') - length(replace('cross_section_height', ' ', '')) <> length('cross_section_width') - length(replace('cross_section_width', ' ', ''))
 UNION ALL
-SELECT 'v2_impervious_surface' as table_name,
-v2_impervious_surface.id as id,
-'WARNING: impervious surface is not in mapping table' as error
-FROM v2_impervious_surface
-WHERE v2_impervious_surface.id  NOT IN(
-SELECT impervious_surface_id
-FROM v2_impervious_surface_map
-)
-UNION ALL
-SELECT 'v2_impervious_surface_map' as table_name,
-v2_impervious_surface_map.id as id,
+SELECT 'surface_map' as table_name,
+surface_map.id as id,
 'ERROR: impervious surface map refers to non-existent node' as error
-FROM v2_impervious_surface_map
-WHERE v2_impervious_surface_map.connection_node_id  NOT IN(
+FROM surface_map
+WHERE surface_map.connection_node_id  NOT IN(
 SELECT id
-FROM v2_connection_nodes
+FROM connection_node
 )
 UNION ALL
-SELECT 'v2_surface' as table_name,
-v2_surface.id as id,
+SELECT 'surface' as table_name,
+surface.id as id,
 'WARNING: surface is not in mapping table' as error
-FROM v2_surface
-WHERE v2_surface.id  NOT IN(
+FROM surface
+WHERE surface.id  NOT IN(
 SELECT surface_id
-FROM v2_surface_map
+FROM surface_map
 )
 UNION ALL
-SELECT 'v2_impervious_surface_map' as table_name,
-v2_impervious_surface_map.id as id,
-'WARNING: impervious surface map is not in impervious surface layer' as error
-FROM v2_impervious_surface_map
-WHERE v2_impervious_surface_map.id  NOT IN(
+SELECT 'surface_map' as table_name,
+surface_map.id as id,
+'WARNING: surface map is not in surface layer' as error
+FROM surface_map
+WHERE surface_map.id  NOT IN(
 SELECT id
-FROM v2_impervious_surface
+FROM surface
 )
 UNION ALL
-SELECT 'v2_connection_nodes' as table_name,
-v2_connection_nodes.id as id,
+SELECT 'connection_node' as table_name,
+connection_node.id as id,
 'WARNING: connection node without impervious surface' as error
-FROM v2_connection_nodes
-WHERE v2_connection_nodes.id IN(
-SELECT v2_impervious_surface_map.connection_node_id
-FROM v2_impervious_surface_map
-WHERE v2_impervious_surface_map.impervious_surface_id NOT IN(
-SELECT v2_impervious_surface.id
-FROM v2_impervious_surface
+FROM connection_node
+WHERE connection_node.id IN(
+SELECT surface_map.connection_node_id
+FROM surface_map
+WHERE surface_map.surface_id NOT IN(
+SELECT surface.id
+FROM surface
 ))
 UNION ALL
-SELECT 'v2_channel' as table_name,
-v2_channel.id as id,
+SELECT 'channel' as table_name,
+channel.id as id,
 'ERROR: channel without cross section location' as error
-FROM v2_channel
-LEFT JOIN v2_cross_section_location
-ON v2_channel.id = v2_cross_section_location.channel_id
-WHERE v2_cross_section_location.id IS NULL
+FROM channel
+LEFT JOIN cross_section_location
+ON channel.id = cross_section_location.channel_id
+WHERE cross_section_location.id IS NULL
 UNION ALL
-SELECT 'v2_cross_section_location' as table_name,
-v2_cross_section_location.id as id,
-'ERROR: cross section location without definition' as error
-FROM v2_cross_section_location
-LEFT JOIN v2_cross_section_definition
-ON v2_cross_section_location.definition_id = v2_cross_section_definition.id
-WHERE v2_cross_section_definition.id IS NULL
+SELECT 'cross_section_location' as table_name,
+cross_section_location.id as id,
+'ERROR: cross section location has no definition shape' as error
+FROM cross_section_location
+WHERE cross_section_shape IS NULL
 UNION ALL
-SELECT 'v2_culvert' as table_name,
-v2_culvert.id as id,
-'ERROR: culvert without cross section definition' as error
-FROM v2_culvert
-LEFT JOIN v2_cross_section_definition
-ON v2_culvert.cross_section_definition_id = v2_cross_section_definition.id
-WHERE v2_cross_section_definition.id IS NULL
+SELECT 'culvert' as table_name,
+culvert.id as id,
+'ERROR: culvert has no definition shape' as error
+FROM culvert
+WHERE cross_section_shape IS NULL
 UNION ALL
-SELECT 'v2_weir' as table_name,
-v2_weir.id as id,
-'ERROR: weir without cross section definition' as error
-FROM v2_weir
-LEFT JOIN v2_cross_section_definition
-ON v2_weir.cross_section_definition_id = v2_cross_section_definition.id
-WHERE v2_cross_section_definition.id IS NULL
+SELECT 'weir' as table_name,
+weir.id as id,
+'ERROR: weir has no definition shape' as error
+FROM weir
+WHERE cross_section_shape IS NULL
 UNION ALL
-SELECT 'v2_orifice' as table_name,
-v2_orifice.id as id,
-'ERROR: orifice without cross section definition' as error
-FROM v2_orifice
-LEFT JOIN v2_cross_section_definition
-ON v2_orifice.cross_section_definition_id = v2_cross_section_definition.id
-WHERE v2_cross_section_definition.id IS NULL
+SELECT 'orifice' as table_name,
+orifice.id as id,
+'ERROR: orifice has no definition shape' as error
+FROM orifice
+WHERE cross_section_shape IS NULL
 UNION ALL
-SELECT 'v2_manhole' as table_name,
-v2_manhole.id as id,
-'ERROR: manhole without connection node' as error
-FROM v2_manhole
-LEFT JOIN v2_connection_nodes
-ON v2_manhole.connection_node_id = v2_connection_nodes.id
-WHERE v2_connection_nodes.id IS NULL
-UNION ALL
-SELECT 'v2_1d_boundary_conditions' as table_name,
-v2_1d_boundary_conditions.id as id,
+SELECT 'boundary_condition_1d' as table_name,
+boundary_condition_1d.id as id,
 'WARNING: 1d boundary without connection node' as error
-FROM v2_1d_boundary_conditions
-LEFT JOIN v2_connection_nodes
-ON v2_1d_boundary_conditions.connection_node_id = v2_connection_nodes.id
-WHERE v2_connection_nodes.id IS NULL
+FROM boundary_condition_1d
+LEFT JOIN connection_node
+ON boundary_condition_1d.connection_node_id = connection_node.id
+WHERE connection_node.id IS NULL
 UNION ALL
-SELECT 'v2_cross_section_location' as table_name,
-v2_cross_section_location.id as id,
+SELECT 'cross_section_location' as table_name,
+cross_section_location.id as id,
 'ERROR: cross section location without reference level' as error
-FROM v2_cross_section_location
+FROM cross_section_location
 WHERE reference_level IS NULL
 UNION ALL
-SELECT 'v2_connection_nodes' as table_name,
-v2_connection_nodes.id as id,
+SELECT 'connection_node' as table_name,
+connection_node.id as id,
 'WARNING: connection node without initial waterlevel' as error
-FROM v2_connection_nodes
-WHERE initial_waterlevel IS NULL
+FROM connection_node
+WHERE initial_water_level IS NULL
 UNION ALL
-SELECT 'v2_connection_nodes' as table_name,
-v2_connection_nodes.id as id,
+SELECT 'connection_node' as table_name,
+connection_node.id as id,
 'ERROR: node without connection' as error
-FROM v2_connection_nodes
-WHERE v2_connection_nodes.id NOT IN (SELECT connection_node_start_id
-FROM v2_pumpstation
-WHERE connection_node_start_id IS NOT NULL
+FROM connection_node
+WHERE connection_node.id NOT IN (SELECT connection_node_id
+FROM pump
+WHERE connection_node_id IS NOT NULL
 UNION ALL
-SELECT connection_node_end_id
-FROM v2_pumpstation
-WHERE connection_node_end_id IS NOT NULL
+SELECT connection_node_id_end
+FROM pump_map
+WHERE connection_node_id_end IS NOT NULL
 UNION ALL
-SELECT connection_node_start_id
-FROM v2_channel
-WHERE connection_node_start_id IS NOT NULL
+SELECT connection_node_id_start
+FROM channel
+WHERE connection_node_id_start IS NOT NULL
 UNION ALL
-SELECT connection_node_end_id
-FROM v2_channel
-WHERE connection_node_end_id IS NOT NULL
+SELECT connection_node_id_end
+FROM channel
+WHERE connection_node_id_end IS NOT NULL
 UNION ALL
-SELECT connection_node_start_id
-FROM v2_culvert
-WHERE connection_node_start_id IS NOT NULL
+SELECT connection_node_id_start
+FROM culvert
+WHERE connection_node_id_start IS NOT NULL
 UNION ALL
-SELECT connection_node_end_id
-FROM v2_culvert
-WHERE connection_node_end_id IS NOT NULL
+SELECT connection_node_id_end
+FROM culvert
+WHERE connection_node_id_end IS NOT NULL
 UNION ALL
-SELECT connection_node_start_id
-FROM v2_pipe
-WHERE connection_node_start_id IS NOT NULL
+SELECT connection_node_id_start
+FROM pipe
+WHERE connection_node_id_start IS NOT NULL
 UNION ALL
-SELECT connection_node_end_id
-FROM v2_pipe
-WHERE connection_node_end_id IS NOT NULL
+SELECT connection_node_id_end
+FROM pipe
+WHERE connection_node_id_end IS NOT NULL
 UNION ALL
-SELECT connection_node_start_id
-FROM v2_weir
-WHERE connection_node_start_id IS NOT NULL
+SELECT connection_node_id_start
+FROM weir
+WHERE connection_node_id_start IS NOT NULL
 UNION ALL
-SELECT connection_node_end_id
-FROM v2_weir
-WHERE connection_node_end_id IS NOT NULL
+SELECT connection_node_id_end
+FROM weir
+WHERE connection_node_id_end IS NOT NULL
 UNION ALL
-SELECT connection_node_start_id
-FROM v2_orifice
-WHERE connection_node_start_id IS NOT NULL
+SELECT connection_node_id_start
+FROM orifice
+WHERE connection_node_id_start IS NOT NULL
 UNION ALL
-SELECT connection_node_end_id
-FROM v2_orifice
-WHERE connection_node_end_id IS NOT NULL
+SELECT connection_node_id_end
+FROM orifice
+WHERE connection_node_id_end IS NOT NULL
 )
 UNION ALL
-SELECT 'v2_pumpstation' as table_name,
-v2_pumpstation.id as id,
+SELECT 'pump' as table_name,
+pump.id as id,
 'WARNING: start level for pump station not close to initial waterlevel' as error
-FROM v2_pumpstation
-LEFT JOIN v2_connection_nodes
-ON v2_pumpstation.connection_node_start_id = v2_connection_nodes.id
-WHERE abs(v2_connection_nodes.initial_waterlevel - v2_pumpstation.start_level) > 0.05
+FROM pump
+LEFT JOIN connection_node
+ON pump.connection_node_id = connection_node.id
+WHERE abs(connection_node.initial_water_level - pump.start_level) > 0.05
 UNION ALL
-SELECT 'v2_cross_section_definition' as table_name,
-v2_cross_section_definition.id as id,
-'ERROR: shape or width is not defined' as error
-FROM v2_cross_section_definition
-WHERE shape IS NULL OR width IS NULL OR width = ''
-UNION ALL
-SELECT 'v2_pumpstation' as table_name,
-v2_pumpstation.id as id,
+SELECT 'pump' as table_name,
+pump.id as id,
 'ERROR: pumpstation from and to the same connection node' as error
-FROM v2_pumpstation
-WHERE v2_pumpstation.connection_node_start_id = v2_pumpstation.connection_node_end_id
+FROM pump
+LEFT JOIN pump_map
+ON pump_map.pump_id = pump.id
+WHERE pump.connection_node_id = pump_map.connection_node_id_end
 UNION ALL
-SELECT 'v2_weir' as table_name,
-v2_weir.id as id,
+SELECT 'weir' as table_name,
+weir.id as id,
 'ERROR: weir from and to the same connection node' as error
-FROM v2_weir
-WHERE v2_weir.connection_node_start_id = v2_weir.connection_node_end_id
+FROM weir
+WHERE weir.connection_node_id_start = weir.connection_node_id_end
 UNION ALL
-SELECT 'v2_orifice' as table_name,
-v2_orifice.id as id,
+SELECT 'orifice' as table_name,
+orifice.id as id,
 'ERROR: orifice from and to the same connection node' as error
-FROM v2_orifice
-WHERE v2_orifice.connection_node_start_id = v2_orifice.connection_node_end_id
+FROM orifice
+WHERE orifice.connection_node_id_start = orifice.connection_node_id_end
 UNION ALL
-SELECT 'v2_culvert' as table_name,
-v2_culvert.id as id,
+SELECT 'culvert' as table_name,
+culvert.id as id,
 'ERROR: culvert from and to the same connection node' as error
-FROM v2_culvert
-WHERE v2_culvert.connection_node_start_id = v2_culvert.connection_node_end_id
+FROM culvert
+WHERE culvert.connection_node_id_start = culvert.connection_node_id_end
 UNION ALL
-SELECT 'v2_weir' as table_name,
-v2_weir.id as id,
-'WARNING: start level ' || v2_weir.crest_level || ' not close to initial waterlevel ' || conn1.initial_waterlevel || ', ' || conn2.initial_waterlevel as error
-FROM v2_weir
-LEFT JOIN v2_connection_nodes as conn1
-ON v2_weir.connection_node_start_id = conn1.id
-LEFT JOIN v2_connection_nodes as conn2
-ON v2_weir.connection_node_end_id = conn2.id
-WHERE abs(conn1.initial_waterlevel - v2_weir.crest_level) > 0.05 AND abs(conn2.initial_waterlevel - v2_weir.crest_level) > 0.05 AND v2_weir.crest_level != 15
+SELECT 'weir' as table_name,
+weir.id as id,
+'WARNING: start level ' || weir.crest_level || ' not close to initial waterlevel ' || conn1.initial_water_level || ', ' || conn2.initial_water_level as error
+FROM weir
+LEFT JOIN connection_node as conn1
+ON weir.connection_node_id_start = conn1.id
+LEFT JOIN connection_node as conn2
+ON weir.connection_node_id_end = conn2.id
+WHERE abs(conn1.initial_water_level - weir.crest_level) > 0.05 AND abs(conn2.initial_water_level - weir.crest_level) > 0.05 AND weir.crest_level != 15
 UNION ALL
-SELECT 'v2_channel' as table_name,
-v2_channel.id as id,
-'WARNING: initial water level at start and end node are not equal ' || conn1.initial_waterlevel || ', ' || conn2.initial_waterlevel as error
-FROM v2_channel
-LEFT JOIN v2_connection_nodes as conn1
-ON v2_channel.connection_node_start_id = conn1.id
-LEFT JOIN v2_connection_nodes as conn2
-ON v2_channel.connection_node_end_id = conn2.id
-WHERE conn1.initial_waterlevel != conn2.initial_waterlevel
+SELECT 'channel' as table_name,
+channel.id as id,
+'WARNING: initial water level at start and end node are not equal ' || conn1.initial_water_level || ', ' || conn2.initial_water_level as error
+FROM channel
+LEFT JOIN connection_node as conn1
+ON channel.connection_node_id_start = conn1.id
+LEFT JOIN connection_node as conn2
+ON channel.connection_node_id_end = conn2.id
+WHERE conn1.initial_water_level != conn2.initial_water_level
 UNION ALL
-SELECT 'v2_orifice' as table_name,
-v2_orifice.id as id,
-'WARNING: initial water level at start and end node are not equal ' || conn1.initial_waterlevel || ', ' || conn2.initial_waterlevel as error
-FROM v2_orifice
-LEFT JOIN v2_connection_nodes as conn1
-ON v2_orifice.connection_node_start_id = conn1.id
-LEFT JOIN v2_connection_nodes as conn2
-ON v2_orifice.connection_node_end_id = conn2.id
-WHERE conn1.initial_waterlevel != conn2.initial_waterlevel AND v2_orifice.discharge_coefficient_positive != 0 AND v2_orifice.discharge_coefficient_negative != 0 AND v2_orifice.crest_level < max(conn1.initial_waterlevel, conn2.initial_waterlevel)
+SELECT 'orifice' as table_name,
+orifice.id as id,
+'WARNING: initial water level at start and end node are not equal ' || conn1.initial_water_level || ', ' || conn2.initial_water_level as error
+FROM orifice
+LEFT JOIN connection_node as conn1
+ON orifice.connection_node_id_start = conn1.id
+LEFT JOIN connection_node as conn2
+ON orifice.connection_node_id_end = conn2.id
+WHERE conn1.initial_water_level != conn2.initial_water_level AND orifice.discharge_coefficient_positive != 0 AND orifice.discharge_coefficient_negative != 0 AND orifice.crest_level < max(conn1.initial_water_level, conn2.initial_water_level)
 UNION ALL
-SELECT 'v2_culvert' as table_name,
-v2_culvert.id as id,
-'WARNING: initial water level at start and end node are not equal ' || conn1.initial_waterlevel || ', ' || conn2.initial_waterlevel as error
-FROM v2_culvert
-LEFT JOIN v2_connection_nodes as conn1
-ON v2_culvert.connection_node_start_id = conn1.id
-LEFT JOIN v2_connection_nodes as conn2
-ON v2_culvert.connection_node_end_id = conn2.id
-WHERE conn1.initial_waterlevel != conn2.initial_waterlevel AND v2_culvert.discharge_coefficient_positive != 0 AND v2_culvert.discharge_coefficient_negative != 0 AND max(v2_culvert.invert_level_start_point, v2_culvert.invert_level_end_point) < max(conn1.initial_waterlevel, conn2.initial_waterlevel)
+SELECT 'culvert' as table_name,
+culvert.id as id,
+'WARNING: initial water level at start and end node are not equal ' || conn1.initial_water_level || ', ' || conn2.initial_water_level as error
+FROM culvert
+LEFT JOIN connection_node as conn1
+ON culvert.connection_node_id_start = conn1.id
+LEFT JOIN connection_node as conn2
+ON culvert.connection_node_id_end = conn2.id
+WHERE conn1.initial_water_level != conn2.initial_water_level AND culvert.discharge_coefficient_positive != 0 AND culvert.discharge_coefficient_negative != 0 AND max(culvert.invert_level_start, culvert.invert_level_end) < max(conn1.initial_water_level, conn2.initial_water_level)
 UNION ALL
-SELECT 'v2_impervious_surface_map' as table_name,
-v2_impervious_surface_map.id as id,
+SELECT 'surface_map' as table_name,
+surface_map.id as id,
 'WARNING: percentage = 100 and should be 14.4 or 11.5' as error
-FROM v2_impervious_surface_map
-WHERE v2_impervious_surface_map.percentage = 100
+FROM surface_map
+WHERE surface_map.percentage = 100
 UNION ALL
-SELECT 'v2_control_table' as table_name,
-v2_control_table.id as id,
+SELECT 'table_control' as table_name,
+table_control.id as id,
 'ERROR: action_table has more than 1000 characters (model will crash)' as error
-FROM v2_control_table
-WHERE length(v2_control_table.action_table) > 1000
+FROM table_control
+WHERE length(table_control.action_table) > 1000
 
