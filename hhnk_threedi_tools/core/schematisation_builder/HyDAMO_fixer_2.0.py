db1dd133eff4149ba23333c09efb9cd5af200cdc
Esther van der Laan <esthervdl5@gmail.com>
2025-12-31 16:09:51 +0100

    summary_val_fix.gpgk begin

--- hhnk_threedi_tools/core/schematisation_builder/HyDAMO_fixer_2.0.py
+++ hhnk_threedi_tools/core/schematisation_builder/HyDAMO_fixer_2.0.py
@@ -34,17 +34,91 @@ class Hydamo_fixer:
         with open(resources_fixconfig_path, "r") as f:
             self.fix_config = json.load(f)
 
+        # define path to results.gpkg of validation
+        self.validation_results_gpkg_path = validation_directory_path / "results" / "results.gpkg"
+
+        self.report_gpkg_path = self.validation_directory_path / "fix_tussenstappen" / "summary_val_fix.gpkg"
+
     def create_validation_fix_reports(self):
-        standard_columns = ["CODE", "valid", "invalid_critical", "invalid_warning", "ignored"]
-        for rules_layer in self.validation_rules["objects"]:
-            layer_name = rules_layer["object"]
-            layer_specific_columns = standard_columns.copy()
+        for layer in self.fix_config["objects"]:
+            layer_name = layer["object"]
 
-            # define validation columns for table
-            # for rule in rules_layer['validation_rules']:
-            # print(rule)
-            # TODO: add in validation_rules.json a variable: "validated_attribute"
-            # TODO: take this variable and add it to layer_specific_columns
+            # fill in standard columns based on validation results
+            val_results_layer_gdf = gpd.read_file(self.validation_results_gpkg_path, layer=layer_name)
 
-            # read hydamo layer
-            # hydamo_layer_gdf = gpd.read_file(self.hydamo_gpkg_path, layer=layer_name)
+            layer_report_gdf = val_results_layer_gdf[
+                ["code", "geometry", "valid", "invalid_critical", "invalid_non_critical", "ignored"]
+            ]
+
+            # remove rows where invalid columns are empty strings
+            layer_report_gdf["invalid_critical"] = layer_report_gdf["invalid_critical"].replace("", None)
+            layer_report_gdf["invalid_non_critical"] = layer_report_gdf["invalid_non_critical"].replace("", None)
+            layer_report_gdf = layer_report_gdf.dropna(subset=["invalid_critical", "invalid_non_critical"], how="all")
+
+            # add layer specifivc columns based on fix config
+            for fix in layer["fixes"]:
+                attribute_name = fix["attribute_name"]
+                if attribute_name not in list(layer_report_gdf.columns):
+                    # add columns to layer report gdf
+                    layer_report_gdf[attribute_name] = None
+                    layer_report_gdf[f"validation_sum_{attribute_name}"] = None
+                    layer_report_gdf[f"fixes_{attribute_name}"] = None
+                    layer_report_gdf[f"manual_overwrite_{attribute_name}"] = None
+
+            for index, row in layer_report_gdf.iterrows():
+                # connect IDs of invalid_critical to validation_id in fix config
+                if row["invalid_critical"] is not None or row["invalid_non_critical"] is not None:
+                    invalid_ids = []
+                    if row["invalid_critical"] is not None:
+                        invalid_ids += [int(x) for x in row["invalid_critical"].split(";")]
+                    if row["invalid_non_critical"] is not None:
+                        invalid_ids += [int(x) for x in row["invalid_non_critical"].split(";")]
+
+                    for fix in layer["fixes"]:
+                        validation_id = fix["validation_id"]
+                        attribute_name = fix["attribute_name"]
+                        fix_id = fix["fix_id"]
+                        fix_name = fix["fix_name"]
+
+                        if validation_id in invalid_ids:
+                            # define validation sum text
+                            if fix["error_type"] == "critical":
+                                text_val_sum = f"C{validation_id}:{fix['error_message']}"
+                            else:
+                                text_val_sum = f"W{validation_id}:{fix['error_message']}"
+
+                            # fill in the validation sum column
+                            current_val_sum = layer_report_gdf.at[index, f"validation_sum_{attribute_name}"]
+                            if current_val_sum is None:
+                                layer_report_gdf.at[index, f"validation_sum_{attribute_name}"] = text_val_sum
+                            else:
+                                layer_report_gdf.at[index, f"validation_sum_{attribute_name}"] = (
+                                    f"{current_val_sum};{text_val_sum}"
+                                )
+
+                            # define fix suggestion text
+                            if fix["fix_type"] == "automatic":
+                                text_fix_suggestion = f"AF{fix_id}:{fix_name}"
+                            else:
+                                text_fix_suggestion = f"MF{fix_id}:{fix_name}"
+                            # TODO: als er een aanname of berekning is, deze ook toevoegen aan fix suggestion
+
+                            # fill in fix suggestion column
+                            current_fix = layer_report_gdf.at[index, f"fixes_{attribute_name}"]
+                            if current_fix is None:
+                                layer_report_gdf.at[index, f"fixes_{attribute_name}"] = text_fix_suggestion
+                            else:
+                                layer_report_gdf.at[index, f"fixes_{attribute_name}"] = (
+                                    f"{current_fix};{text_fix_suggestion}"
+                                )
+                            # TODO: fill met attribute value if present
+
+            # remove columns with no data
+            for col in layer_report_gdf.columns:
+                # TODO: attribute en manual overwrite moeten blijven staan
+                if col not in ["code", "geometry", "valid", "invalid_critical", "invalid_non_critical", "ignored"]:
+                    if layer_report_gdf[col].isnull().all():
+                        layer_report_gdf = layer_report_gdf.drop(columns=[col])
+            # save layer report gdf to gpkg
+
+            layer_report_gdf.to_file(self.report_gpkg_path, layer=layer_name, driver="GPKG")
