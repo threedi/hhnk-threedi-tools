6f5f82f67213f28de925430bdc27061a9977042e
jzitman <90759839+jzitman@users.noreply.github.com>
2026-03-05 14:33:54 +0100

    validation styling (#343)
    * Added styling to validation output
    
    Added "traffic light" styling to validation results. A styling geopackage is used as template. Also uploaded newer versions of DAMO, HyDAMO, raw_export geopackages.
    
    * automatic changes
    
    * Updated styling to account for ignored validation rules
    
    * reset styling to treat ignored as invalid_critical
    
    * Removed geopackages to prevent conflicts

--- hhnk_threedi_tools/core/schematisation_builder/main.py
+++ hhnk_threedi_tools/core/schematisation_builder/main.py
@@ -145,6 +145,10 @@ class SchematisationBuilder:
             if not validation_rules_json_path.exists():
                 shutil.copyfile(resources_validationrules_path, validation_rules_json_path)
 
+            validation_styling_path = resources_validationrules_path = hrt.get_pkg_resource_path(
+                schematisation_builder_resources, "styling.gpkg"
+            )
+
             if not Path(coverage_location).exists():
                 raise FileNotFoundError(
                     f"Coverage data for validation not found in {coverage_location}. Please provide location with index.shp and tiles to this location."
@@ -154,6 +158,7 @@ class SchematisationBuilder:
                 hydamo_file_path=self.hydamo_file_path,
                 validation_rules_json_path=validation_rules_json_path,
                 validation_directory_path=validation_directory_path,
+                template_file_path=validation_styling_path,
                 coverages_dict={"AHN": coverage_location},
                 output_types=["geopackage"],
             )
