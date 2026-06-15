a1fafc5f45fabef5664c1b5b18bbc697494b8089
jcacosta89 <78262040+jcacosta89@users.noreply.github.com>
2026-06-15 14:00:39 +0200

    Update breach_checks.py

--- hhnk_threedi_tools/breaches/breach_checks.py
+++ hhnk_threedi_tools/breaches/breach_checks.py
@@ -1,19 +1,37 @@
+<<<<<<< Updated upstream
+=======
+# %%
+>>>>>>> Stashed changes
 import shutil
 from pathlib import Path
 
 import geopandas as gpd
 import threedigrid_builder.application as app
 
+<<<<<<< Updated upstream
 sqlite_path = Path(r"H:\02.modellen\midden_zuid_pytest\work in progress\schematisation\midden_zuid_pytest.sqlite")
 dem_path = Path(
     r"H:\02.modellen\midden_zuid_pytest\work in progress\schematisation\rasters\dem_schemer_midden_zuid_fix_compressed_v2.tif"
 )
 out_path = Path(r"H:\02.modellen\midden_zuid_pytest\work in progress\schematisation\gridadmin_midden_zuid_pytest.gpkg")
+=======
+sqlite_path = Path(r"H:\02.modellen\midden_zuid_pytest\work in progress\schematisation\midden_zuid_pytest.gpkg")
+dem_path = Path(
+    r"H:\02.modellen\midden_zuid_pytest\work in progress\schematisation\rasters\dem_schemer_midden_zuid_fix_compressed_v2.tif"
+)
+out_path = Path(
+    r"H:\02.modellen\midden_zuid_pytest\work in progress\schematisation\gridadmin_midden_zuid_pytest_v2.gpkg"
+)
+>>>>>>> Stashed changes
 convert_to_geopackage = True
 tmp_folder = sqlite_path.parent / "tmp"
 tmp_folder.mkdir(exist_ok=True)
 
+<<<<<<< Updated upstream
 temp_sqlite_path = sqlite_path.parent / "tmp" / (sqlite_path.stem + "_copy.sqlite")
+=======
+temp_sqlite_path = sqlite_path.parent / "tmp" / (sqlite_path.stem + "_copy.gpkg")
+>>>>>>> Stashed changes
 
 shutil.copy(sqlite_path, temp_sqlite_path)
 
@@ -29,3 +47,8 @@ app.make_gridadmin(
 shutil.rmtree(tmp_folder)
 
 grid_read_nodes = gpd.read_file(out_path, layer="nodes")
+<<<<<<< Updated upstream
+=======
+
+# %%
+>>>>>>> Stashed changes
