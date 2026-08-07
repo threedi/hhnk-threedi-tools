e5fc6561cef05ddaec98e9182de092e9eb9d3386
jcacosta89 <78262040+jcacosta89@users.noreply.github.com>
2026-08-07 11:18:37 +0200

    Update gui_helpers.py

--- hhnk_threedi_tools/core/vergelijkingstool/gui/gui_helpers.py
+++ hhnk_threedi_tools/core/vergelijkingstool/gui/gui_helpers.py
@@ -1,30 +1,34 @@
-from ipywidgets import Button, HBox, Label, Text, VBox
+from pathlib import Path
 from typing import Any
+
+from ipywidgets import Button, HBox, Label, Text, VBox
+
 from hhnk_threedi_tools.core.vergelijkingstool.utils import get_model_info
 
-from pathlib import Path
 
 def update_output_folder(change):
-        """Update model path"""
-        try:
-            model_info = get_model_info(change["new"])  
-            output_folder_text.value = str(model_info.output_folder)
-            
-        except Exception as e:
-            output_folder_text.value = "model could not be read"
-            print(e)
+    """Update model path"""
+    try:
+        model_info = get_model_info(change["new"])
+        output_folder_text.value = str(model_info.output_folder)
 
-def update_output_file_path(change= None):
-        """Update model path"""
-        try:
-            output_folder = Path(output_folder_text.value)
-            output_name_input_text = output_name_input.value
-            full_path = str(output_folder / output_name_input_text)
-            output_file_path.value = full_path
-            
-        except Exception as e:
-            output_file_path.value = "Path has not been update"
-            print(e)
+    except Exception as e:
+        output_folder_text.value = "model could not be read"
+        print(e)
+
+
+def update_output_file_path(change=None):
+    """Update model path"""
+    try:
+        output_folder = Path(output_folder_text.value)
+        output_name_input_text = output_name_input.value
+        full_path = str(output_folder / output_name_input_text)
+        output_file_path.value = full_path
+
+    except Exception as e:
+        output_file_path.value = "Path has not been update"
+        print(e)
+
 
 # function use to make the gui dinamic.
 def on_compare_change(change: Any) -> None:
