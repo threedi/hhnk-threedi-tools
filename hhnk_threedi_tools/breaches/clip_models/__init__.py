8adcef3a84cdcd94f537fc55368a69d839dc15bb
jcacosta89 <78262040+jcacosta89@users.noreply.github.com>
2026-08-20 18:26:29 +0200

    ruff

--- hhnk_threedi_tools/breaches/clip_models/__init__.py
+++ hhnk_threedi_tools/breaches/clip_models/__init__.py
@@ -1,6 +1,6 @@
 """Tools for creating clipped 3Di submodels."""
 
-from hhnk_threedi_tools.breaches.clip_models.create_clip_models. import run
+from hhnk_threedi_tools.breaches.clip_models.create_clip_models import run
 from hhnk_threedi_tools.breaches.clip_models.submodel_constants import SchematisationType
 
 __all__ = [
